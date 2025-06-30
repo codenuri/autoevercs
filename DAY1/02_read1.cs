58613e3a30bc0896d56337eaf36774d0637cb90b
ecourse <smkang@codenuri.co.kr>
2025-06-30 12:27:00 +0900

    Add files via upload

---
+++ DAY1/02_read1.cs
@@ -0,0 +1,32 @@
+using System;
+
+// 핵심 : 문자열 입력　(15page ~)
+
+// C# 내부
+/*
+namespace System
+{
+    class Console
+    {
+        // "Console 용어" : 명령 프롬프트 창을 예전에 부르던 이름
+        //                  (콘솔창, 터미널 등의 이름 사용)
+
+        // 이 클래스안에 "콘솔창" 으로 부터 입출력할때 사용하는
+        // 다양한 메소드(함수) 제공. 
+    }
+}
+*/
+
+// 출력 : Console.Write() 또는 Console.WriteLine()
+Console.Write("input your name >> ");
+
+
+// 입력 : Console.Read() 또는 Console.ReadLine()
+string s = Console.ReadLine();  
+
+
+Console.WriteLine(s);  // 변수값 출력 - 변수 시간에 자세히
+      
+  
+  
+   
