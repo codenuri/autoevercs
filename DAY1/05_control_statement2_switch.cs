5eae227f17dbd32edadf0f29df5ddc68cba6e79d
ecourse <smkang@codenuri.co.kr>
2025-06-30 17:20:29 +0900

    Add files via upload

--- DAY1/05_control_statement2_switch.cs
+++ DAY1/05_control_statement2_switch.cs
@@ -16,12 +16,24 @@ switch( num )
         Console.WriteLine("two");
         break;
 
-    default:
-        Console.WriteLine("other");
-        break;
+    // default 는 생략 가능
+//    default:
+//        Console.WriteLine("other");
+//        break;
 }
 
+// C# 의 switch 는 C보다 아주 막강합니다.
+// => switch 패턴 매칭이라는 분야가 있습니다.
+object obj = 3.4; // object : 모든 타입의 값을 담는 도구
 
-object obj = 3.4;
+switch (obj)
+{
+    // 값이 아닌 타입 조사
+    case int:    Console.WriteLine("int"); break;
+    case double: Console.WriteLine("double"); break;
+
+    // 값도 조사
+    case "ABCD": Console.WriteLine("ABCD"); break;
+}
 
 
