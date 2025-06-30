066df513e7e5309fbd54870e0a4c24f384767939
ecourse <smkang@codenuri.co.kr>
2025-06-30 11:35:11 +0900

    Add files via upload

---
+++ DAY1/00_entry3.cs
@@ -0,0 +1,16 @@
+﻿// Top-Level 방식의 원리(6 page)
+
+// 아래 코드를
+// System.Console.WriteLine("hello, C#");
+
+
+// 컴파일러가 아래 처럼 변경한 것
+
+class CompilerGeneratedName // <= 클래스 이름은 컴파일러가 임의로 결정
+{
+    public static void Main()
+    {
+        // 이 위치에 사용자가 만든 코드를 넣게 됩니다.
+        System.Console.WriteLine("hello, C#");
+    }
+}
---
+++ DAY1/00_entry4.cs
@@ -0,0 +1,16 @@
+﻿// 00_entry4.cs
+
+// C# 은 "함수 오버로딩" 을 지원합니다.
+// =>  "인자 타입이 다르면" 동일 이름의 함수를 여러개 만들수 있다.
+
+void foo(int a)
+{
+}
+
+void foo(double a)
+{
+}
+
+System.Console.WriteLine("A");
+
+// entry1~3 번 빌드 막고 컴파일 해보세요
