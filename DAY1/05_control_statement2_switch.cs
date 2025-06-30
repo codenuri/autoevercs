b4e017959e31786b82b554546141ecf4e7dd148a
ecourse <smkang@codenuri.co.kr>
2025-06-30 17:15:54 +0900

    Add files via upload

---
+++ DAY1/05_control_statement2_switch.cs
@@ -0,0 +1,27 @@
+using System;
+
+// 핵심 : switch 문
+// if     : 참/거짓에 의한 분기
+// switch : 값에 의한 분기
+
+int num = 1;
+
+switch( num )
+{
+    case 1: 
+        Console.WriteLine("one");
+        break;
+
+    case 2:
+        Console.WriteLine("two");
+        break;
+
+    default:
+        Console.WriteLine("other");
+        break;
+}
+
+
+object obj = 3.4;
+
+
