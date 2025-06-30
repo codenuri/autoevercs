54cc4df8ec5b756a6c6b9b234f28be5c307cf379
ecourse <smkang@codenuri.co.kr>
2025-06-30 16:28:34 +0900

    Add files via upload

--- DAY1/03_variable5.cs
+++ DAY1/03_variable5.cs
@@ -29,18 +29,29 @@ Console.WriteLine($"a = {a}, b = {b}, a+b = {a + b}");
 // #2. casting 이야기
 int n1 = 0;     // n1 정수를 담는 4바이트 크기 "박스"
 
-n1 = 3.4; // ? 
+n1 = 3.4; // C/C++ : 문제없다, 단, 3.4 가 아닌 3이 들어간다.
+          //         0.4 의 데이타 손실
+          //         C/C++의 단점중 한개, 너무오래전에 만든 언어
+          //         C : 1960년대 탄생, C++ : 1983 년
+
+            // C#, Java, Rust : error
+            //                  단, 캐스팅하면 가능
+
+            // Python         : ok 
+            //                  0과 3.4 는 다른 메모리
+            //                  Post It
+
+// C# 에서 실수값을 정수형 메모리에 담으려면 캐스팅 필요
+n1 = (int)3.4;// ok
 
 
 
 
-
-// nameof
+// 3. nameof
 int color = 100;
-string s = "abcd";
-string name = nameof(color);
 
-WriteLine($"{nameof(color)} : {color}");				
-*/
+WriteLine($"{color}");
+WriteLine($"{nameof(color)}");
+
 
 
