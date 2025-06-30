2d967f7d8fd2dc9589eb7d80e3dafd8c7e9269ba
ecourse <smkang@codenuri.co.kr>
2025-06-30 17:22:50 +0900

    Add files via upload

---
+++ DAY1/expression.cs
@@ -0,0 +1,13 @@
+﻿// expression.cs
+
+// 모든 프로그래밍 언어에서 사용되는 개념
+// statement vs expression
+
+// statement : 실행단위가 되는 한줄의 문장
+//             C/C++/C#/Java 의 세미콜론(;) 으로 끝나는 코드
+
+// expression : 하나의 값을 만드는 코드
+
+int n = 3;
+
+int a = (n + 2) * 3;
