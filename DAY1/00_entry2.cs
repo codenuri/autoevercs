be3f17f113bafaee86b125f7472ad69ad1c8e55d
ecourse <smkang@codenuri.co.kr>
2025-06-30 11:29:39 +0900

    Add files via upload

---
+++ DAY1/00_entry2.cs
@@ -0,0 +1,9 @@
+﻿// C# 9.0 이후 부터는 Main 을 만들지 않으면 
+// 자동으로 소스 코드의 1번째 문장 부터 실행
+// => C# 에서는 Top Level Programming 방식이라고 합니다.
+
+System.Console.WriteLine("hello, C#");
+
+// 강의에서는
+// 간단한 예제는 "Top-Level 방식" 으로
+// 복잡한 예제는 "Main 메소드 작성 방식" 으로
