00f193c76cc375bbf43b0211f345925a8641e633
ecourse <smkang@codenuri.co.kr>
2025-06-30 12:08:10 +0900

    Add files via upload

--- DAY1/01_using3.cs
+++ DAY1/01_using3.cs
@@ -1,6 +1,15 @@
 //using System; // 현재 파일에서만 System 이름 없이 사용가능
-global using System; // 프로젝트내의 모든 파일에 적용됨
+//global using System; // 프로젝트내의 모든 파일에 적용됨
                      // (단, 이소스도 같이 빌드에 포함되어야 함)
 
+// global.cs 때문에 아래 처럼 사용가능합니다.
+// => global.cs 와 using3.cs 빌드 포함되어야 합니다.
 Console.WriteLine("AAA");
 
+// visual studio 2022 이전
+// => 사용자가 직접 global.cs 같은 파일을 만들어야 했습니다
+
+// visual studio 2022 이후
+// => 프로젝트 생성시 자동으로 "global.cs" 유사 파일이 추가됩니다
+// => DAY1//obj//debug//net8.0 에서
+// => DAY1.globalusing.g.cs 열어 보세요
---
+++ DAY1/01_using4.cs
@@ -0,0 +1,16 @@
+﻿// 01_using4.cs
+// 정리
+
+// #1. 예전에는 아래와 같은 코드를 작성해야 했습니다.
+// using System;
+
+// #2. 하지만 visual studio 2022 부터는
+// => // => DAY1//obj//debug//net8.0/DAY1.globalusing.g.cs 때문에
+// => 위코드 없어도 아래 처럼 사용가능합니다.
+Console.WriteLine("AA");
+
+
+// #3. 단 아래처럼 Console 도 제거하려면
+// 사용자가 직접 "using static System.Console" 해야 합니다.
+WriteLine("A"); // 현재는 에러
+
