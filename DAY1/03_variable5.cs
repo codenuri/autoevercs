e9d45772977d9d9b557027ce4683505a7093cfce
ecourse <smkang@codenuri.co.kr>
2025-06-30 16:12:39 +0900

    Add files via upload

---
+++ DAY1/03_variable5.cs
@@ -0,0 +1,36 @@
+using static System.Console;
+
+// 03_variable5.cs
+
+// #1. 변수값 출력 방법(9 page)
+int a = 3;
+double b = 3.4;
+
+// 1. 변수값 한개 출력
+
+Console.WriteLine(a);
+
+
+// 2. 여러개 변수를 서식에 맞게 출력
+Console.WriteLine("a = {0}, b = {1}, a = {0}", a, b);
+
+
+
+
+
+
+/*
+// casting
+double d = 3.4;
+int n1 = d;		// ?
+
+
+// nameof
+int color = 100;
+string s = "abcd";
+string name = nameof(color);
+
+WriteLine($"{nameof(color)} : {color}");				
+*/
+
+
