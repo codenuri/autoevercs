410e52887b08a7cb077b82150af056bb54380197
ecourse <smkang@codenuri.co.kr>
2025-06-30 16:18:59 +0900

    Add files via upload

--- DAY1/03_variable5.cs
+++ DAY1/03_variable5.cs
@@ -15,11 +15,12 @@ Console.WriteLine(a); // 3
 Console.WriteLine("a = {0}, b = {1}, a = {0}", a, b);
 
 
-// 3. C# 6.0 부터 보간문자열($로시작하는 문자열) 사용가능
+// 3. C# 6.0 부터 보간문자열($로시작하는 문자열, Interpolated string) 사용가능
 
 Console.WriteLine($"a = {a}, b = {b}, a+b = {a + b}");
 
-
+// 참고 : 파이썬
+//println("a = {0}, b = {1}, a = {0}" % (a, b));
 
 
 
