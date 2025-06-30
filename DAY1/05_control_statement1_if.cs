b713823ca2217a47d123a1ed9dd2b2a68fd22f0b
ecourse <smkang@codenuri.co.kr>
2025-06-30 17:07:26 +0900

    Add files via upload

---
+++ DAY1/05_control_statement1_if.cs
@@ -0,0 +1,25 @@
+using static System.Console;
+
+// 제어문(67page ~ )
+
+// 모든 프로그램은 "약속된 entry point" 에서 부터
+// 순차적으로 실행됩니다.
+// 1. 실행흐름을 변경하려면 조건문(conditional statement) 사용
+// 2. 실행흐름을 반복하려면 반복문(iterative   statement) 사용
+
+// 조건문
+// C 언어  : if, switch
+// C# 언어 : if, switch
+// Python : if, match(3.12 버전에서 추가, switch 와 유사) 
+
+// 반복문
+// C    : while, do-while, for
+// C#   : while, do-while, for, foreach
+// Python : while, for(C# foreach와 유사)
+
+
+// 핵심 : if 문
+
+int score = 75;
+
+
