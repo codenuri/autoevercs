4df94a4bc0a567f9994c9cddb2c2b93f29b2d160
ecourse <smkang@codenuri.co.kr>
2025-07-01 14:12:09 +0900

    Add files via upload

---
+++ DAY2/03_value_vs_reference3.cs
@@ -0,0 +1,31 @@
+using static System.Console;
+
+
+class CPoint
+{
+    private int x;
+    private int y;
+    public CPoint(int a, int b) { x = a; y = b; }
+}
+struct SPoint
+{
+    private int x;
+    private int y;
+    public SPoint(int a, int b) { x = a; y = b; }
+}
+class Program
+{
+    public static void Main()
+    {
+        // 38page 그림 참고
+        CPoint cp1;
+        cp1.x = 10;
+
+
+        SPoint sp1;
+        sp1.x = 10;
+
+
+    }
+}
+
