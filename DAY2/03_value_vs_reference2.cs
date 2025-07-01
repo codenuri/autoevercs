d4c5d7738f7764bef5526dc9ea8348eb9b92ffaf
ecourse <smkang@codenuri.co.kr>
2025-07-01 14:07:58 +0900

    Add files via upload

--- DAY2/03_value_vs_reference2.cs
+++ DAY2/03_value_vs_reference2.cs
@@ -17,15 +17,27 @@ class Program
 {
     public static void Main()
     {
-        CPoint cp1 = new CPoint(1, 1);
-        CPoint cp2 = cp1;
+        // 아래 코드의 메모리 그림이 37 page 그림 입니다.
+        // => 반드시 이해해야 합니다.
 
-        cp2.x = 2;
+        CPoint cp1 = new CPoint(1, 1); // 객체가 힙에 생성 
+                                        // cp1이 가리키게 됨
 
-        WriteLine(cp1.x); // ??
+        CPoint cp2 = cp1;       // cp2 는 cp1 과 같은 객체 공유
 
+        cp2.x = 2;  // 공유된 객체 변경
 
-  
+        WriteLine(cp1.x); // 1
+        
+        //-----------------------------
+        SPoint sp1 = new SPoint(1, 1);
+
+        SPoint sp2 = sp1;
+
+        sp2.x = 2; 
+
+        WriteLine(sp1.x); // 1
+
 
     }
 }
