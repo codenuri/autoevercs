2e4f136e3a91146d66efd89084a22ffa6a1ba92b
ecourse <smkang@codenuri.co.kr>
2025-07-01 11:57:43 +0900

    Add files via upload

---
+++ DAY2/wpf2.cs
@@ -0,0 +1,17 @@
+﻿// wpf2.cs
+using System.Windows;
+
+class Program
+{
+    // GUI 프로그램은 Main 위에 아래 코드 필요 - 어려운 내용.
+    // 항상 사용하므로 외우면됩니다.
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+
+        w.Show();
+
+//        MessageBox.Show("Hello");
+    }
+}
---
+++ DAY2/wpf3.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+
+        w.Show();
+
+        // UI(window)를 생성한경우
+        // 프로그램을 종료되지 않도록하게
+        // UI 에서 발생하는 이벤트의 기본 처리를 제공 해야 합니다.
+        // => Application 이라는 클래스가 제공
+        Application app = new Application();
+        app.Run();
+
+    }
+}
