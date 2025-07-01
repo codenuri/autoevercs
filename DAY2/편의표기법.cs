f93c2e1f1cfe02c673bbd5d60011593d5a6941dc
ecourse <smkang@codenuri.co.kr>
2025-07-01 14:32:01 +0900

    Add files via upload

---
+++ DAY2/편의표기법.cs
@@ -0,0 +1,10 @@
+﻿// 편의표기법.cs
+
+// 1. C# 에서 모든 객체는 "new" 로 만들어야 합니다.
+// => ValueType 이건, Refernce Type 이건 모두
+CPoint cp = new CPoint(1, 2);
+SPoint sp = new SPoint(1, 2);
+
+
+// #2. int, string 도 마찬가지 입니다.
+
