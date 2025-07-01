653cd26121033ac814e1d81f24e25d2371f5909d
ecourse <smkang@codenuri.co.kr>
2025-07-01 11:43:38 +0900

    Add files via upload

---
+++ DAY2/wpf1.cs
@@ -0,0 +1,16 @@
+﻿// wpf1.cs
+
+// C# 은 수백 ~ 수천개의 미리 만들어진 클래스를 제공합니다.
+// => 표준 라이브러리(BCL, Base Class Library) 라고 합니다.
+// => 그중에서 UI 를 책임지는 클래스가 "WPF 라이브러리" 입니다.
+
+// WPF 를 사용해 봅시다.
+// 1. TopLevel Program 은 안됨. Main Method 방식
+
+class wpf
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
