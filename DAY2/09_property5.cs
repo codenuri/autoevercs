4dd9771a8e36b2e993f3b692926dccae02a32124
ecourse <smkang@codenuri.co.kr>
2025-07-01 17:43:13 +0900

    Add files via upload

---
+++ DAY2/09_property5.cs
@@ -0,0 +1,22 @@
+﻿// 읽기 전용 property
+
+class Person
+{
+    private int age = 0;
+
+    public int Age
+    {
+        get { return age; }
+//        set { age = value; }
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Person p1 = new Person();
+
+        p1.Age = 10;
+        int n = p1.Age;
+    }
+}
