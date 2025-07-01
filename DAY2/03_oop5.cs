ff734d0bce2f895e312b66709e19aea2323055bb
ecourse <smkang@codenuri.co.kr>
2025-07-01 12:22:33 +0900

    Add files via upload

---
+++ DAY2/03_oop5.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using static System.Console;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = 10;
+        n = 20;
+
+
+        Rectangle rc = new Rectangle(1, 1, 10, 10);
+
+    }
+}
