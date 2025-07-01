1377dd411176d169907b847cd5b53d77ffd09f10
ecourse <smkang@codenuri.co.kr>
2025-07-01 16:30:45 +0900

    Add files via upload

---
+++ DAY2/07_private3.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+class Bike
+{
+    private int gear = 0;
+
+    public void SetGear(int value)
+    {
+        gear = value;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Bike b = new Bike();
+                
+//        b.gear = -10;  // error. private 필드는 접근 안됨.
+        b.SetGear(-10);  // ok
+    }
+}
+
+
