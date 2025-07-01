c6f9b8bba2af83709c58cd11f228137ab66ca8c4
ecourse <smkang@codenuri.co.kr>
2025-07-01 16:24:10 +0900

    Add files via upload

--- DAY2/07_private.cs
+++ DAY2/07_private.cs
@@ -9,16 +9,23 @@
 
 
 // 접근 지정자
+// private : 자신의 메소드만 접근 가능
+// public  : 어디서도 접근 가능
 class Bike
 {
-    int gear = 0;
+    // field : 객체의 상태를 나타내는 데이타
+   
+    private int color = 0;
+    public  int gear = 0;  
 }
-
 class Program
 {
     public static void Main()
     {
         Bike b = new Bike();
+
+        b.color = 10; // error. private 은 외부(Bike 메소드 아닌곳) 접근 안됨
+        b.gear = 10;  // ok.    public Bike 메소드가 아닌곳도 접근 가능
     }
 }
 
