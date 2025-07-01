96c4d97e60dac5164265fa341bcc2e1b70305e7a
ecourse <smkang@codenuri.co.kr>
2025-07-01 09:57:48 +0900

    Add files via upload

---
+++ DAY2/01_method_property4.cs
@@ -0,0 +1,30 @@
+﻿// 교재 31 page 위부분
+
+// #1. C/C++/Java 에서 타입의 종류
+
+// primitive type   : int, double 같이 언어 자체가 제공하는 기본 타입
+// user define type : struct 또는 class 문법을 사용해서 사용자가 만든 타입
+
+// #2. primitive type 은 데이타만 보관한다. 메소드(함수)가 없다.
+int n = 10; // n 은 메소드 없음.
+
+
+// #3. C++/Java 에는 struct, class 문법으로 사용자가 타입 만들수 있습니다.
+class Integer
+{
+    // 데이타와 함수
+    public string ToString() { }
+}
+
+// 사용자 타입은 메소드가 있습니다.
+Integer n = new Integer();
+n.ToString();
+
+
+// #4. C#이 최초 등장하면서 가장 강조한 것!!!
+// => 모든 것은 "객체" 이다.
+// => 모든 타입인 class 또는 struct 로 설계되었다.
+// => int, double 등도 C# 에서는 struct 또는 class 로 설계!!
+// => 그래서 모든 타입의 변수는 메소드가 있다
+
+
