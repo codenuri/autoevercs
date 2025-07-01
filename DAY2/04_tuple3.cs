37235e9bde2490b388b6032ae058d236a3466e89
ecourse <smkang@codenuri.co.kr>
2025-07-01 10:42:44 +0900

    Add files via upload

--- DAY2/04_tuple3.cs
+++ DAY2/04_tuple3.cs
@@ -22,20 +22,27 @@ z = t1.Item3;
 
 // deconstruction 할때
 // => 변수를 선언후 해도 되고, 선언과 동시에 해도 됩니다.
-
+/*
 int a1, b1, c1;
 (a1, b1, c1) = t1;
 
-(int a2, int b2, int b3) = t1;
+(int a2, int b2, int c2) = t1; // 위와 동일
+*/
 
 
 
+// #3. 아래 2줄의 차이점은 ? - 49page 아래
+(int a1, int a2, int a3) tp = (1, 2, 3); 
+        // tuple 을 생성 한것
+        // tp 는 tuple 인데 멤버의 이름이 a1, a2, a3
+        // 즉, a1, a2, a3 는 변수 가 아닌 tuple 멤버이름
 
+(int b1, int b2, int b3)    = (4, 5, 6);
+                    // (4, 5, 6) 이라는 tuple 이 값을 
+                    // 각각, b1, b2, b3 라는 변수에 담은것
+                    // deconstruction
+                    // b1,b2, b3 는 각각 int 변수!
 
-
-
-
-// #3. 아래 2줄의 차이점은 ?
-(int a1, int a2, int a3) t2 = (1, 2, 3); 
-(int b1, int b2, int b3)    = (4, 5, 6); 
+int b1 = 10; // error. 위에서 int b1 선언됨
+int a1 = 10; // ok.    위에 int a1 은 변수 선언 아님
 
