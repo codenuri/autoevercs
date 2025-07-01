b4a6ed51fe6dc75fc019f6786e6555b831a375d1
ecourse <smkang@codenuri.co.kr>
2025-07-01 15:17:57 +0900

    Add files via upload

---
+++ DAY2/string2.cs
@@ -0,0 +1,13 @@
+﻿// string2.cs
+
+// string        : immutable 한 문자열 타입
+// StringBuilder : mutable 한 문자열 타입. 
+
+string s = "ABCD";
+s[0] = 'X'; // error. immutable
+
+
+// StringBuilder : 편의 표기법 없음. new 사용해야됨(45 page)
+StringBuilder sb = new StringBuilder("ABCD");
+
+sb[0] = 'X'; // ok
