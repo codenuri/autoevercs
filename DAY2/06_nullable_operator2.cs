a75a2720eb99f1a773a4a766ed5a8beb4d0e30f5
ecourse <smkang@codenuri.co.kr>
2025-07-01 16:15:40 +0900

    Add files via upload

--- DAY2/06_nullable_operator2.cs
+++ DAY2/06_nullable_operator2.cs
@@ -25,3 +25,16 @@ var ret4 = s2?.ToString();
 // 즉, s2 가 null 이 아닐때만 메소드 호출해 달라.
 
 
+// #4. 
+int[] arr = null; // 배열도 reference 타입
+
+int n1 = arr[0];  // error
+int n2 = arr?[0]; // arr != null 일때만 요소 꺼내달라.
+                  // arr == null 이면 요소 접근하지 말고, null 반환
+                  // error. n2 가 int 이므로 null 담을수 없다.
+
+int? n3 = arr?[0]; // ok!!
+
+// 핵심 : 아래 코드는 많이 사용합니다.
+// 객체이름.메소드() : 객체가 null 이면 에러
+// 객체이름?.메소드() : 객체가 null 이 아닐때만 호출. 
