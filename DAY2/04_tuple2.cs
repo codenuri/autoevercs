6c49c22c096bb4a59bcae83f10fa26cfd4604c00
ecourse <smkang@codenuri.co.kr>
2025-07-01 10:18:43 +0900

    Add files via upload

---
+++ DAY2/04_tuple2.cs
@@ -0,0 +1,27 @@
+using static System.Console;
+
+// #1. tuple 타입과 생성 방법
+int n = 0;
+
+int[] arr = { 1, 2, 3 };   // 배열 타입   : Type[]
+                           // 배열 초기화 : {} 사용 
+
+(int, int, int) t1 = (1, 2, 3); // 튜플 타입  : (Type1, Type2, Type3)
+                                // 튜플 초기화: = (값, 값, 값)
+
+
+? t2 = (1, 3.4, "ABCD");
+
+
+
+
+
+// #2. 요소 접근
+var t2 = (1, 2, 3);
+
+
+
+// #3. Named Member
+(int,     int,     int)       v3 = (1,2,3); 
+
+
