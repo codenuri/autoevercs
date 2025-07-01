9753a2ad1eaa9da093e7dcb8294e62b066dbfd2e
ecourse <smkang@codenuri.co.kr>
2025-07-01 11:26:55 +0900

    Add files via upload

--- DAY2/02_oop3.cs
+++ DAY2/02_oop3.cs
@@ -19,14 +19,20 @@ class Rect
     // 생성자 : 클래스 이름과 동일한 함수(메소드)
     //          변수를 생성할때 자동으로 호출된다.
     //          데이타(field)초기화를 위해 사용
+    //          반환 타입도 표기 하지 않습니다.
     public Rect(int a, int b, int c, int d)
     {
-        x1 = a;
-        y1 = b;
-        x2 = c;
-        y2 = d;
+//        x1 = a;
+//        y1 = b;
+//        x2 = c;
+//        y2 = d;
+
+        // 위 4줄과 아래 한줄은 동일합니다.
+        // => tuple deconstruction
+        (x1, y1, x2, y2) = (a, b, c, d);
     }
 }
+
 class Program
 {
     public static void Main()
