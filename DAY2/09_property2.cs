90e99bcba17d9d80c738ce4a1189519c0bfbc8b6
ecourse <smkang@codenuri.co.kr>
2025-07-01 17:12:02 +0900

    Add files via upload

--- DAY2/09_property2.cs
+++ DAY2/09_property2.cs
@@ -1,8 +1,14 @@
 class Person
 {
     private int age;
-
-
+    
+    // 아래 Age 는 ()가 없으므로 "필드" 처럼 보입니다.
+    // 그런데, {} 가 있으므로 "메소드" 처럼 보입니다.
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
 }
 
 class Program
@@ -11,5 +17,7 @@ class Program
     {
         Person p1 = new Person();
 
+        p1.Age = 10;
+        int n = p1.Age;
     }
 }
