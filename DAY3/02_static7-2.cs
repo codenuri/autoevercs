2020f60e721131707913931464da29126ac3ee7f
ecourse <smkang@codenuri.co.kr>
2025-07-02 11:54:26 +0900

    Add files via upload

--- DAY3/02_static7-2.cs
+++ DAY3/02_static7-2.cs
@@ -41,8 +41,26 @@ class Date
     }
     //----------------------------------------------
     // 이번 핵심 소스는 여기부터!!
+    public bool IsLeapYear() 
+    {
+        // 멤버(필드)인 year 가 윤년이지 조사
+        bool b = (year % 400 == 0)
+                 || ((year % 4 == 0) && (year % 100 != 0));
+
+        return b;
+    }
+
+    public static bool IsLeapYear(int y)
+    {
+        // 인자 y 가 윤년이지 조사
+        bool b = (y % 400 == 0)
+                 || ((y % 4 == 0) && (y % 100 != 0));
+
+        return b;
+    }
 
 }
+
 
 class Program
 {
@@ -54,9 +72,19 @@ class Program
         // 어떤 방법이 좋을까요 ?
 
         // #1. instance method
+        // => 객체가 존재 했다면 아래 처럼 편하게 사용
+        // => 객체가 없던 상태에서 조사하려면, 조사를 위해 객체를 생성해야 한다
         bool b1 = today.IsLeapYear();
 
+
         // #2. static method
+        // => 객체가 없어도 조사
         bool b2 = Date.IsLeapYear(2025);
+
+        //=> 객체가 있었다면 아래 처럼
+ //     bool b2 = Date.IsLeapYear(today.Year);
+
+        // IsLeapYear()는 static method 가 좋은데
+        // => istance, static 2개 모두 제공하는 경우도 있습니다.
     }
 }
