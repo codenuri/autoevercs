7d5fa5b8fc4175ddd92eac2e82cb91a55a1848c7
ecourse <smkang@codenuri.co.kr>
2025-07-02 10:33:35 +0900

    Add files via upload

--- DAY3/02_static5.cs
+++ DAY3/02_static5.cs
@@ -11,18 +11,29 @@ class Car
         speed = s;
         ++cnt;
     }
-
-    public int GetCount() { return cnt; }
-}
+    /*
+    public int GetCount()   // instance method
+    {                       // => "객체.메소드()" 로 호출
+        return cnt;         // => "객체가 있어야만" 호출가능
+    }
+    */
+    public static int GetCount() // static method 
+    {                            // => "클래스이름.메소드()"로 호출
+        return cnt;              // => 객체가 없어도 호출가능
+    }                            // => static field는 객체가 없어도 존재 하므로
+}                                //    여기에 접근하기 위해
 
 class Program
 {
     public static void Main()
     {
+        // 여기서도 자동차 갯수를 구하고 싶다.??
+        WriteLine($"{Car.GetCount()}");
+
         Car c1 = new Car(50);
         Car c2 = new Car(80);
 
-        WriteLine($"{c1.GetCount()}");
+        WriteLine($"{Car.GetCount()}");
 
     }
 }
