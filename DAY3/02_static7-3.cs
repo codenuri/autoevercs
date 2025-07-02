6925f636a2e680f49f8829ddd6c3be0d6a4dbbc7
ecourse <smkang@codenuri.co.kr>
2025-07-02 12:15:24 +0900

    Add files via upload

--- DAY3/02_static7-3.cs
+++ DAY3/02_static7-3.cs
@@ -19,6 +19,18 @@ class Program
     public static void Main()
     {
         // 오늘 날짜로 초기화된 "today" 라는 객체를 만들어 보세요
+
+        // 방법 #1
+        // => 아래 처럼 만들면 미래에 실행하면 오늘 날짜가 아니다.
+        Date today = new Date(2025, 7, 2);
+
+
+        // 방법 #2. 아래 처럼 만들어야 한다.
+
+        // int y = 올해의 년도를 구하는 프로그램 코드;
+        // int m = 현재월 구하는 프로그램 코드;
+        // int d = 현재일 구하는 프로그램 코드;
+        Date today = new Date(y, m, d);
     }
 }
 
