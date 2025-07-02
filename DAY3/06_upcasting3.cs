bdb81552377d5a09ff5f81b54aa5c6d856cbbd72
ecourse <smkang@codenuri.co.kr>
2025-07-02 17:37:39 +0900

    Add files via upload

--- DAY3/06_upcasting3.cs
+++ DAY3/06_upcasting3.cs
@@ -3,17 +3,17 @@
 // GUI 라이브러리를 설계해 봅시다.
 class Window
 {
-//  List<Button> child_list = new List<Button>(); // 버튼 밖에 보관할수 없다
+    //  List<Button> child_list = new List<Button>(); // 버튼 밖에 보관할수 없다
 
-//  List<object> child_list = new List<object>(); // 모든 객체를 보관할수 있다.
-                                                // int 도 보관된다. ???
-                                                // 자식윈도우는 UI 만 허용해야 한다.
+    //  List<object> child_list = new List<object>(); // 모든 객체를 보관할수 있다.
+                                                    // int 도 보관된다. ???
+                                                    // 자식윈도우는 UI 만 허용해야 한다.
 
+    List<UIElement> child_list = new List<UIElement>();
 
-
-    public void AddChild(Button btn)
+    public void AddChild(UIElement element)
     {
-        child_list.Add(btn); // 자식컨트롤 보관
+        child_list.Add(element); // 자식컨트롤 보관
     }
 }
 
@@ -26,8 +26,10 @@ class Window
 class UIElement
 {
     // 모든 UI의 공통의 특징을 설계
+    private int backgroundColor;
+    private int foregroundColor;
 }
-
+// 이제 모든 컨트롤은 UIElement 에서 상속 받아야 합니다.
 class Button : UIElement
 { 
 }
