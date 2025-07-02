c524e1da076c30a1c08ecfbec163e156c5d00fb7
ecourse <smkang@codenuri.co.kr>
2025-07-02 09:43:04 +0900

    Add files via upload

---
+++ DAY3/01_this3.cs
@@ -0,0 +1,30 @@
+﻿// 01_this3
+
+// this 활용
+// this 는 언제 사용하나요 ?
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+
+    public void SetX(int a)
+    {
+        x = a;
+    }
+    public void SetY(int a)
+    {
+        y = a;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p = new Point();
+
+        p.SetX(1);
+        p.SetY(2);
+    }
+}
