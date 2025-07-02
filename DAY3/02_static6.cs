48f02f52b513c6df8ad4fdb002451118988a233c
ecourse <smkang@codenuri.co.kr>
2025-07-02 11:04:14 +0900

    Add files via upload

---
+++ DAY3/02_static6.cs
@@ -0,0 +1,18 @@
+﻿// 02_static6.cs
+using static System.Console;
+
+// 프로그램에서 날짜 를 많이 사용한다.
+// => "Date" 라는 타입을 설계 하자
+
+class Date
+{
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Date d1 = new Date();
+    }
+}
