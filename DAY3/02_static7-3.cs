82c8e959e23bdd4491bc23fed51175521455b1de
ecourse <smkang@codenuri.co.kr>
2025-07-02 12:12:10 +0900

    Add files via upload

---
+++ DAY3/02_static7-3.cs
@@ -0,0 +1,24 @@
+﻿// 02_static7-3.cs ==> static6번복사해오세요
+
+using static System.Console;
+
+class Date
+{
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(int y, int m, int d)
+    {
+        (year, month, day) = (y, m, d);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        // 오늘 날짜로 초기화된 "today" 라는 객체를 만들어 보세요
+    }
+}
+
