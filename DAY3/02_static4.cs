b828851ef79b6b82254db3dd2e723f20a0b15606
ecourse <smkang@codenuri.co.kr>
2025-07-02 10:25:03 +0900

    Add files via upload

---
+++ DAY3/02_static4.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+
+class Car
+{
+    private int speed = 0;
+
+//    public static int cnt = 0;  
+    private static int cnt = 0;
+
+    public Car(int s)
+    {
+        speed = s;
+        ++cnt;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car(50);
+        Car c2 = new Car(80);
+
+//      Car.cnt = -10; // 사용자가 실수 했다.
+                       // 이렇게 할수 없게 해야 한다.
+                       // static field 라도 보호해야 한다
+                       // private 사용
+
+//      WriteLine($"{Car.cnt}");
+    }
+}
+
+
