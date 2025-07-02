4f0d14e9f585381d2d325e535665b30ffdaaa52d
ecourse <smkang@codenuri.co.kr>
2025-07-02 17:06:55 +0900

    Add files via upload

---
+++ DAY3/06_upcasting2.cs
@@ -0,0 +1,24 @@
+﻿class Animal
+{
+    public int age = 0;
+}
+
+class Dog : Animal
+{
+    public int color = 0;
+}
+
+class Cat : Animal
+{
+    public int speed = 0;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Animal a = new Dog();
+
+        a.color = 10; // error. Animal 타입인 a 로는 Dog 고유멤버 접근안됨
+    }
+}
