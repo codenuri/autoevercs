4aca9abb8fe6a27cdcac2da4ba4f4cefbaa635ea
ecourse <smkang@codenuri.co.kr>
2025-07-02 11:19:28 +0900

    Add files via upload

---
+++ DAY3/02_static7.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+class Date
+{
+
+    private int year;
+    private int month;
+    private int day;
+
+    public int Month
+    {
+        get { return month; }
+        set
+        {
+            if (value < 1 || value > 12)
+                throw new Exception(); 
+
+            month = value;
+        }
+    }
+
+    public Date(int y, int m, int d)
+    {
+        (year, month, day) = (y, m, d);
+    }
+    //----------------------------------------------
+    // 이번 핵심 소스는 여기부터!!
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Date d1 = new Date(2025, 7, 2);
+    }
+}
