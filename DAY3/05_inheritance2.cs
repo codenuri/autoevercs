8127ec95fdd2199084330dfadbe1f37f289e874c
ecourse <smkang@codenuri.co.kr>
2025-07-02 16:19:32 +0900

    Add files via upload

---
+++ DAY3/05_inheritance2.cs
@@ -0,0 +1,16 @@
+﻿// C# 의 모든 타입은 자동으로 "object" 라는 클래스에 상속됩니다.
+
+// class Car : object   // 아래 코드는코드는 컴파일러에 의해서 이렇게 됩니다.
+class Car    
+{
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c = new Car();
+
+        c.
+    }
+}
