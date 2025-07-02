aa69212d76713206f835d459c639bf0b94016c4c
ecourse <smkang@codenuri.co.kr>
2025-07-02 17:28:40 +0900

    Add files via upload

--- DAY3/06_upcasting3.cs
+++ DAY3/06_upcasting3.cs
@@ -1,11 +1,16 @@
 ﻿// 왜 ??? Dog 객체를 Animal 참조에 담나요 ??
 // 그냥 Dog 참조에 담으면 안되나요 ?
-
 // GUI 라이브러리를 설계해 봅시다.
-
 class Window
 {
+    List<Button> child_list = new List<Button>();
+
+    public void AddChild(Button btn)
+    {
+        child_list.Add(btn); // 자식컨트롤 보관
+    }
 }
+
 class Button 
 { 
 }
@@ -22,5 +27,8 @@ class Program
         Button b2 = new Button();
         w.AddChild(b1);
         w.AddChild(b2);
+
+        TextBox tb = new TextBox();
+        w.AddChild(tb); // ??
     }
 }
