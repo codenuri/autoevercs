8759056a6499e9aeea41e6454634c9e22b0a5da1
ecourse <smkang@codenuri.co.kr>
2025-07-02 17:12:37 +0900

    Add files via upload

--- DAY3/06_upcasting2.cs
+++ DAY3/06_upcasting2.cs
@@ -18,14 +18,17 @@ class Program
     public static void Main()
     {
         // #1. a 는 객체 자체는 아니고 힙에 있는 객체를 가리키는 참고 입니다.
-        Animal a = new Dog();
+//      Animal a = new Dog();
+        Animal a = new Cat();
 
 
         // #2. Animal 타입인 a 로는 Dog 고유멤버 접근안됨
-//      a.color = 10; // error. 
+        //      a.color = 10; // error. 
 
 
         // #3. Dog 고유 멤버에 접근하려면 a 를 다시 Dog 참조로
         Dog d = (Dog)a;
+
+        d.color = 10; // ok
     }
 }
