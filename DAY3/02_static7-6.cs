7fa57cdd82fdf1640670f3f3fe0dc3878d7ef87c
ecourse <smkang@codenuri.co.kr>
2025-07-02 12:30:41 +0900

    Add files via upload

---
+++ DAY3/02_static7-6.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void Main()
+    {
+        // C# 에 이미 날짜 클래스 있습니다.
+        // DateTime : 날짜와 시간을 같이 보관하는 클래스
+
+        DateTime dt1 = new DateTime(2025, 7, 2, 13, 0, 0);
+        WriteLine(dt1);
+
+        // #1. 오늘 날짜로 초기화된 dt2 객체 만들어서 출력해 보세요
+
+        // #2. 오늘 부터 1000일뒤 날짜 만들어서 출력해 보세요
+
+
+        // #3. 올해가 윤년인지 조사해 보세요
+        // => 윤년조사 함수가 instance method 인지 static method 인지 확인해 보세요
+        
+    }
+}
+
+
