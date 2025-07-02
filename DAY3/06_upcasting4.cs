1593908b23ce97a070c1f9d0f3b8abbf1630e430
ecourse <smkang@codenuri.co.kr>
2025-07-02 17:39:27 +0900

    Add files via upload

---
+++ DAY3/06_upcasting4.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+class UIElement
+{
+    private int backgroundColor;
+    private int foregroundColor;
+}
+class Button : UIElement
+{
+}
+class TextBox : UIElement
+{
+}
+
+class Window
+{
+    List<UIElement> child_list = new List<UIElement>();
+
+    public void AddChild(UIElement element)
+    {
+        child_list.Add(element);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Window w = new Window();
+        Button b1 = new Button();
+        Button b2 = new Button();
+        w.AddChild(b1);
+        w.AddChild(b2);
+
+        TextBox tb = new TextBox();
+        w.AddChild(tb); // ??
+    }
+}
