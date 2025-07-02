39f8b962899c16f68dcffcdfe2d7b3d51eef28d2
ecourse <smkang@codenuri.co.kr>
2025-07-02 11:10:07 +0900

    Add files via upload

--- DAY3/02_static6.cs
+++ DAY3/02_static6.cs
@@ -6,6 +6,24 @@ using static System.Console;
 
 class Date
 {
+    // #1. 날짜의 요소를 필드로!
+    // => 날짜는 유효하지 않은 값이있다. private 으로 방어!
+    private int year;
+    private int month;
+    private int day;
+
+    // #2. setter/getter..
+    // => C# 은 property 권장
+    public int Month
+    {
+        get { return month; }
+        set 
+        { 
+            if (value < 0 || value > 12) 
+                return; 
+            month = value; 
+        }
+    }
 
 }
 
