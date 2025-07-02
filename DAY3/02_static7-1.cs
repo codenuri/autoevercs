cd51594e2393cbb4fcc0a093c4cec02d26c53bcd
ecourse <smkang@codenuri.co.kr>
2025-07-02 11:37:10 +0900

    Add files via upload

--- DAY3/02_static7-1.cs
+++ DAY3/02_static7-1.cs
@@ -34,17 +34,33 @@ class Date
     }
     //----------------------------------------------
     // 이번 핵심 소스는 여기부터!!
+    public static int HowManyDays(int m)
+    {
+        if ( m < 1 || m > 12)
+            throw new Exception();
+        
+        // 7월의 날짜수는 days[6] 입니다.
+        return days[m - 1];
+
+        // 위 구현은 2월일때 윤년이 고려되지 않은 구현.. 
+    }
 }
 
 class Program
 {
     public static void Main()
     {
-        Date d1 = new Date(2025, 7, 2);
+
 
         // 7월이 몇일까지 있는지 알고 싶다.
         // 다음중 좋은 코드는 ?
-        int ds = d1.HowManyDays();
+        // => 년월일 의 모든 데이타가 필요한것은 아닙니다.
+        // => 2번이 좋습니다.
+        // 방법 #1. 7월 을 가진 객체를 만들어서 메소드 호출
+//      Date d1 = new Date(2025, 7, 2);
+//      int ds = d1.HowManyDays();
+
+        // 방법 #2. 객체없이 호출가능하게, 인자로 7월 전달
         int ds = Date.HowManyDays(7);
     }
 }
