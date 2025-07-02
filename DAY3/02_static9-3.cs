28fe64de5066a2cafa9b6d8b7938be494334fd72
ecourse <smkang@codenuri.co.kr>
2025-07-02 14:16:53 +0900

    Add files via upload

---
+++ DAY3/02_static9-3.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    private int data = 0;
+
+    public static void Main()
+    {
+        Program pg = new Program();
+        pg.IMethod();
+
+        Program.SMethod();
+    }
+
+    public static void SMethod()
+    {
+        data = 10;
+    }
+
+    public void IMethod() 
+    {
+        data = 10;
+    }
+
+}
