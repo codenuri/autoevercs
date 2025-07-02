29e38da8cae994fe6526977dfc5abc88f33fb9d6
ecourse <smkang@codenuri.co.kr>
2025-07-02 12:46:33 +0900

    Add files via upload

--- DAY3/02_static9-1.cs
+++ DAY3/02_static9-1.cs
@@ -6,10 +6,12 @@
 
     public static void Main()
     {
-        // 자신의 static method 호출
+        // #1. 자신의 static method 호출
         Program.M2();   // ok. "클래스이름.static메소드()"
         M2();           // ok. 자신의 static 메소드는 클래스 이름 생략가능
 
+
+        // #2. 자신의 instance method 호출 이야기.
 //      M1(); // error. instance method 는 객체가 필요한다.
               // Main 은 객체 없이 호출된 함수안에 있는것
 
