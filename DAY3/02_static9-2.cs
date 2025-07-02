800809dc75e9e2720e80cf37f4bc9b01aa1b6e71
ecourse <smkang@codenuri.co.kr>
2025-07-02 14:03:33 +0900

    Add files via upload

---
+++ DAY3/02_static9-2.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static void SMethod() { }
+
+    public void IMethod1() 
+    { 
+    }
+
+    public void IMethod2()
+    {
+
+    }
+}
