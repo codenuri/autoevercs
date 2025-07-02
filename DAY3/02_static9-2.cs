87e29d87d50831b596c25515b96e561d27768f2c
ecourse <smkang@codenuri.co.kr>
2025-07-02 14:07:40 +0900

    Add files via upload

--- DAY3/02_static9-2.cs
+++ DAY3/02_static9-2.cs
@@ -2,7 +2,15 @@
 {
     public static void Main()
     {
+        // #1. static method 는 객체가 없어도 호출가능
+        // => 클래스 이름만 알면 호출가능
+        // => "언제 어디서도 호출가능" 하다는것
+        Program.SMethod();
+        SMethod();      // 같은 클래스 메소드에서는 클래스 이름 생략가능
 
+        // #2. instance method 는 객체 필요
+        Program pg = new Program();
+        pg.IMethod1();
     }
 
     public static void SMethod() { }
