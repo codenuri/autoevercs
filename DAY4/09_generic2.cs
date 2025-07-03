5cbbf65b6312e53b91950cb1942341104b0d8e64
ecourse <smkang@codenuri.co.kr>
2025-07-03 16:23:54 +0900

    Add files via upload

---
+++ DAY4/09_generic2.cs
@@ -0,0 +1,20 @@
+// class 도 generic 가능합니다.
+
+class Point<T>
+{
+    private T x = 0;
+    private T y = 0;
+
+    public Point(T a, T b)
+    {
+        (x, y) = (a, b);
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        // class generic 은 반드시 타입인자 보내야 합니다.
+        Point<int> p1 = new Point<int>(1, 2);
+    }
+}
