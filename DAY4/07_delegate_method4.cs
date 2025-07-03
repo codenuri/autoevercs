8cb7af26b48f32df425f170bd41cad5ecdf7ba5f
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:09:06 +0900

    Add files via upload

---
+++ DAY4/07_delegate_method4.cs
@@ -0,0 +1,28 @@
+using static System.Console;
+
+// multicast 
+
+delegate void MyFunc(int arg);
+
+class Program
+{
+	public static void SMethod(int arg) => WriteLine("Program.SMethod");
+
+	public static void Main()
+	{	
+		// 핵심 : +=을 사용하면 2개이상의 메소드 등록가능합니다
+		MyFunc f = Test.SMethod; 
+		MyFunc f += Program.SMethod;
+
+		f(10); // 2개 호출
+
+		f -= Test.SMethod; // 제거도 가능
+		f(10); 
+    }
+}
+
+class Test
+{
+    public static void SMethod(int arg) => WriteLine("Test.SMethod");
+    public void IMethod(int arg) => WriteLine("Test_Object.IMethod");
+}
