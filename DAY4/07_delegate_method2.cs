fef084f46f94e97d2c4c1fe44005e936c66bcac1
ecourse <smkang@codenuri.co.kr>
2025-07-03 14:49:43 +0900

    Add files via upload

--- DAY4/07_delegate_method2.cs
+++ DAY4/07_delegate_method2.cs
@@ -13,16 +13,26 @@ class Program
     public static void Main()
     {
         // 자신의 static method 등록
-        MyFunc f1 = ?;
-        MyFunc f2 = ?;
+        MyFunc f1 = Program.SMethod;
+        MyFunc f2 = SMethod;
+
 
         // 자신의 instance method 등록
-        MyFunc f3 = ?;
-        MyFunc f4 = ?;
+        // 핵심 : 현재 Main 메소드는 static 이므로 Program 객체(this)가 없다
+        // 객체를 만들어야 IMethod 사용가능
+        Program pg = new Program();
 
+        MyFunc f3 = pg.IMethod;
+//      MyFunc f4 = IMethod; // 될까요 ? error.
+                             // Main 은 static 이므로 this 없습니다
+
+        pg.InstanceMethod();
     }
 
 	public void InstanceMethod()
 	{
-	}
+        // 현재 메소드가 호출되었다는 것은 객체 가 있다는 것
+        MuFunc f1 = this.IMethod; //ok
+        MuFunc f2 = IMethod;      // ok
+    }
 }
