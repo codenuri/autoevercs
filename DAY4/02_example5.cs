17c105f9468fa1abd47b2116b6f19f82f8c9d979
ecourse <smkang@codenuri.co.kr>
2025-07-03 10:26:46 +0900

    Add files via upload

--- DAY4/02_example5.cs
+++ DAY4/02_example5.cs
@@ -2,11 +2,20 @@
 
 // 핵심 #5. 올바른 해결책!
 
+// 1. 모든 파생 클래스(도형)의 공통의 특징은 반드시 기반 클래스(Shape)에도
+//    있어야 한다.
+//    => 문법적인 규칙이 아닌!!
+//    => 디자인 원칙
+//    => 그래야 Shape 참조로 Draw() 호출이 가능해 진다.
+
+
 
 
 class Shape
 {
     public int color = 0;
+
+    public void Draw() { WriteLine("Draw Shape"); }
 }
 
 class Rect : Shape
