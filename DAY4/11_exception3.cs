dc10af3eada87ca8a4d1078c9aff805f3d2d1563
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:59:30 +0900

    Add files via upload

--- DAY4/11_exception3.cs
+++ DAY4/11_exception3.cs
@@ -1,5 +1,18 @@
 ﻿using static System.Console;
 
+// #1. 오류의 대한 자세한 정보를 담을수 있는 class 를 설계 하세요
+// => C# 에서는 Exception 이라는 클래스로 부터 파생 받는 것을 권장
+
+class DBBackupException : Exception
+{
+    // 다양한 멤버를 사용해서 실패 이유를 담을수 있게 하세요
+    private string message;
+
+    public DBBackupException(string s) => message = s;
+}
+
+
+// #2. 메소드가 실패하면 예외 객체를 던지세요(throw)
 
 
 class Database
