923b7b224d1b8f276095c018f67bff4ab7fd576c
ecourse <smkang@codenuri.co.kr>
2025-07-03 11:08:01 +0900

    Add files via upload

---
+++ DAY4/03_ToString1.cs
@@ -0,0 +1,16 @@
+﻿// 기존에 있는 "03_ToString.cs" 를 "03_ToString2.cs" 로 변경하고
+
+// "03_ToString1.cs" 추가해 보세요
+
+class Program
+{
+    public static void Main()
+    {
+        int n = 10;
+        double d = 3.4;
+
+        // 어떤 값(10, 3.4등)을 화면에 출력하려면
+        // => 결국 문자열로 변경해서 출력해야 합니다.
+        // => 10 => "10"
+    }
+}
