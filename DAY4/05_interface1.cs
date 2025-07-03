74d8f084620eb74ee4f972e5d63a6a7702ce0009
ecourse <smkang@codenuri.co.kr>
2025-07-03 12:19:04 +0900

    Add files via upload

---
+++ DAY4/05_interface1.cs
@@ -0,0 +1,36 @@
+using static System.Console;
+
+// 언제 추상클래스를 사용하고, 언제 인터페이스를 사용하나요 ?
+
+// 도형 편집기 Shape
+// => 모든 도형은 color 같은 멤버 필요 합니다.
+// => 추상 클래스가 좋습니다.
+
+// 인터페이스
+// => 함수 이름만 약속하고 싶을때
+// => interface1 ~ 2 예제
+
+
+class Program 
+{
+	public static void Main()
+	{
+		int n1 = 10;
+		int n2 = 20;
+
+		string s1 = "AAA";
+		string s2 = "BBB";
+
+		// 두 변수의 크기를 비교하는 방법. 
+		
+		// #1. 비교 연산자(<, >,...) 사용
+
+
+
+    }
+
+	public static void M1(IComparable ic)
+	{
+	}
+
+}
