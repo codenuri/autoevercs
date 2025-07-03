ed63fac0e61222c14eaa7cda66aef53ecbabce0b
ecourse <smkang@codenuri.co.kr>
2025-07-03 12:02:40 +0900

    Add files via upload

---
+++ DAY4/05_interface0.cs
@@ -0,0 +1,24 @@
+﻿// 05_interface0.cs - example6.cs 복사
+
+using static System.Console;
+
+class Shape
+{
+    public int color = 0;
+
+    public virtual void Draw() { WriteLine("Draw Shape"); }
+}
+
+class Rect : Shape
+{
+    public override void Draw() { WriteLine("Draw Rect"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Shape s = new Rect();
+        s.Draw();
+    }
+}
