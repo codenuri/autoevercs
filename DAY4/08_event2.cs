4c53315a8a29daf743970eae9798d1c7a45f7d7b
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:21:41 +0900

    Add files via upload

--- DAY4/08_event2.cs
+++ DAY4/08_event2.cs
@@ -4,7 +4,9 @@ class Button
 {
     // #1. 아래 코드는 "Handler" 라는 타입을 만든것!
     // => 저장할 메소드 모양(void())
-    delegate void Handler();
+    // => delegate 를 class 안에 만들때는 public 으로 해야
+    //    해당 Delegate 변수를 다시 public 으로 할수 있습니다.
+    public delegate void Handler();
 
     // #2. Handler 타입의 Click 이라는 필드
     public Handler Click = null;
