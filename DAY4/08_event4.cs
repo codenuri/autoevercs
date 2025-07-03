bee54df6044c00397a1fdbbdaf50eeee78f3712c
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:32:11 +0900

    Add files via upload

--- DAY4/08_event4.cs
+++ DAY4/08_event4.cs
@@ -9,6 +9,8 @@ class Button
 
     // 아래 처럼하면 Click 에 대해서 : +=, -= 만 가능. = 안됨
     //                              최초 등록시도 += 만 가능
+
+    // 결국 event 는 "delegate" 를 안전하게 사용하기 위한것
     public event Handler Click = null;
 
     public void UserPressButton()
@@ -26,12 +28,14 @@ class Program
     {
         Button btn1 = new Button();
 
+        
 //        btn1.Click = Foo; // error
         btn1.Click += Foo; // error
         btn1.Click += Goo;
 
         btn1.Click += Hoo; 
 
+        
         
 
         btn1.UserPressButton();
