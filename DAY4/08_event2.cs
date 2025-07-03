96ca6ea9ed8780f9a3ff2f91e9140dd068ddceb1
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:19:53 +0900

    Add files via upload

--- DAY4/08_event2.cs
+++ DAY4/08_event2.cs
@@ -2,8 +2,19 @@ using static System.Console;
 
 class Button
 {
+    // #1. 아래 코드는 "Handler" 라는 타입을 만든것!
+    // => 저장할 메소드 모양(void())
+    delegate void Handler();
+
+    // #2. Handler 타입의 Click 이라는 필드
+    public Handler Click = null;
+
     public void UserPressButton()
     {
+        if (Click != null)
+        {
+            Click();
+        }
 
     }
 }
@@ -17,9 +28,11 @@ class Program
 
         // 버튼을 만든후, 각 버튼을 눌렀을때 호출될 메소드를 등록합니다.
         btn1.Click = Foo;
-        btn1.Click = Goo;
+        btn2.Click = Goo;
 
         btn1.UserPressButton(); 
         btn2.UserPressButton();  		
     }
+    public static void Foo() => WriteLine("Foo");
+    public static void Goo() => WriteLine("Goo");
 }
