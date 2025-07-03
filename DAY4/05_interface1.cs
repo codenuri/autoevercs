a0f63bd9340f58bc66206a87f9af9e60d54fe9f4
ecourse <smkang@codenuri.co.kr>
2025-07-03 12:22:15 +0900

    Add files via upload

--- DAY4/05_interface1.cs
+++ DAY4/05_interface1.cs
@@ -11,6 +11,7 @@ using static System.Console;
 // => interface1 ~ 2 예제
 
 
+
 class Program 
 {
 	public static void Main()
@@ -22,14 +23,25 @@ class Program
 		string s2 = "BBB";
 
 		// 두 변수의 크기를 비교하는 방법. 
-		
+
 		// #1. 비교 연산자(<, >,...) 사용
+		// => 정수, 실수 같은 숫자 타입만 가능
+		bool b1 = n1 < n2; // ok
+		bool b2 = s1 < s2; // error
 
-
+		// #2. CompareTo 메소드 사용
+		// => C# 에서 비교 가능한 모든 타입에는 CompareTo 메소드 있습니다.
+		int ret1 = n1.CompareTo(n2); // n1 > n2 라면 양수 반환 (1 )
+									 // n1 < n2 라면 음수 반환 (-1)
+									 // n1 == n2 라면 0
+		int ret2 = s1.CompareTo(s2);
 
     }
 
-	public static void M1(IComparable ic)
+
+
+
+    public static void M1(IComparable ic)
 	{
 	}
 
