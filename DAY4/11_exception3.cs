8f125484429de5162375d372dfd2b55bd81725c3
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:56:35 +0900

    Add files via upload

---
+++ DAY4/11_exception3.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+
+
+class Database
+{
+    public Database(string dbname) { }
+
+    public bool Backup()
+    {
+        return false;
+    }
+    public void Remove() => WriteLine("Remove DB");
+}
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        Database db = new Database("product.db");
+
+        bool ret = db.Backup();
+
+
+        db.Remove();
+    }
+}
