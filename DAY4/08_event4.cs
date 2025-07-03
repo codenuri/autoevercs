e51a4ec0cd3c2f262a4f33a587158b063cd86799
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:30:13 +0900

    Add files via upload

---
+++ DAY4/08_event4.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+
+class Button
+{
+    public delegate void Handler();
+
+    // 아래 처럼하면 Click 에 대해서 : =, +=, -= 모두 사용가능
+//  public Handler Click = null;
+
+    // 아래 처럼하면 Click 에 대해서 : +=, -= 만 가능. = 안됨
+    //                              최초 등록시도 += 만 가능
+    public event Handler Click = null;
+
+    public void UserPressButton()
+    {
+        if (Click != null)
+        {
+            Click();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Button btn1 = new Button();
+
+//        btn1.Click = Foo; // error
+        btn1.Click += Foo; // error
+        btn1.Click += Goo;
+
+        btn1.Click += Hoo; 
+
+        
+
+        btn1.UserPressButton();
+    }
+    public static void Foo() => WriteLine("Foo");
+    public static void Goo() => WriteLine("Goo");
+    public static void Hoo() => WriteLine("Hoo");
+}
