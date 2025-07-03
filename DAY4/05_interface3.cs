c9e3eba2c8918eb15980ee3e7b822414f0e16ac1
ecourse <smkang@codenuri.co.kr>
2025-07-03 14:07:07 +0900

    Add files via upload

--- DAY4/05_interface3.cs
+++ DAY4/05_interface3.cs
@@ -1,5 +1,9 @@
 using static System.Console;
 
+// 아래 코드는 새로운 카메라(HDCamera) 가 추가될때
+// => Person 의 코드가 수정되었습니다
+// => OCP 를 만족하지 못하는 확장성 없는 설계 입니다
+
 class Camera
 {
     public void Take() { WriteLine("take picture");  }
@@ -13,6 +17,7 @@ class HDCamera
 class Person
 {
     public void UseCamera(Camera c) {  c.Take(); }
+    public void UseCamera(HDCamera c) { c.Take(); }
 }
 
 class Program
@@ -21,7 +26,6 @@ class Program
     {
         Person p = new Person();
         Camera c = new Camera();
-
         p.UseCamera(c);
 
 
