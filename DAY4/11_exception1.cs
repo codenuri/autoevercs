b015fdb534caa42059ddc53911b2303b38ad80e6
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:39:43 +0900

    Add files via upload

--- DAY4/11_exception1.cs
+++ DAY4/11_exception1.cs
@@ -7,6 +7,11 @@ using static System.Console;
 
 // C 언어 : 반환 값으로 실패를 알려 줍니다.
 
+// 단점 : 함수가 실패 했다고 알리는 것을 호출자가 무시 할수도 있다.
+// => 에러 처리의 강제성이 없다.
+// => 심각한 오류를 무시하고 계속 실행되면 더 큰 문제가 발생할수 있다.
+
+
 class Database
 {
     public Database(string dbname) { }
@@ -28,10 +33,13 @@ class Program
 
         bool ret = db.Backup();    
 
+        /*
+        // 사용자가 오류 처리를 안했다!!
         if ( ret == false )
         {
             // 오류 처리.!!
         }
+        */
 
         db.Remove();
     }
