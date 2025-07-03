e3e448155c6cde164d927c8bec9dd591bedaf285
ecourse <smkang@codenuri.co.kr>
2025-07-03 16:15:28 +0900

    Add files via upload

--- DAY4/09_generic1.cs
+++ DAY4/09_generic1.cs
@@ -1,5 +1,6 @@
 ﻿class Program
 {
+    /*
     public static void Swap(ref int a, ref int b)
     {
         int tmp = a;
@@ -12,6 +13,21 @@
         a = b;
         b = tmp;
     }
+    */
+
+    // 위 2개의 Swap 은
+    // => 인자 타입만 다를 뿐 내부 구현은 동일 합니다.
+    // => 이런 경우, 개발자가 직접 만들지 말고,
+    // => 개발자는 "틀(template, generic)" 만 만들고
+    // => 컴파일러가 함수를 만들게 하면 됩니다.
+
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
 
     public static void Main()
     {
