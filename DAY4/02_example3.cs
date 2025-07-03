a77556157cd3bfffccbecb2f65e5f477f99db015
ecourse <smkang@codenuri.co.kr>
2025-07-03 10:07:37 +0900

    Add files via upload

---
+++ DAY4/02_example3.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+class Shape
+{
+    public int color = 0;
+}
+
+class Rect : Shape
+{
+    public void Draw() { WriteLine("Draw Rect"); }
+}
+class Circle : Shape
+{
+    public void Draw() { WriteLine("Draw Circle"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Shape> c = new List<Shape>();
+
+        while (true)
+        {
+            int cmd = int.Parse(ReadLine());
+
+            if (cmd == 1)      { c.Add(new Rect()); }
+            else if (cmd == 2) { c.Add(new Circle()); }
+            else if (cmd == 9)
+            {
+                foreach (var s in c)
+                {
+                    s.Draw();
+                }
+            }
+        }
+    }
+}
+// 위 코드는 어디서 ? 왜 에러일까요 ?
