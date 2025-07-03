fa522d41dfaa3fae4769ebe997020dadde10c83d
ecourse <smkang@codenuri.co.kr>
2025-07-03 17:38:24 +0900

    Add files via upload

--- DAY4/ex3.cs
+++ DAY4/ex3.cs
@@ -3,16 +3,18 @@ using System.Windows.Media;
 
 // 핵심 : 메세지(이벤트) 루프 만들기
 // => Application 클래스 객체를 만들고 Run() 메소드 호출
+// => 관례 : Application 객체는 Window 만들기 전에 생성
 
 class Program
 {
     [STAThread]
     public static void Main()
     {
+        Application app = new Application();
+
         Window w = new Window();
         w.Show();
-
-        Application app = new Application();
+        
         app.Run();
     }
 }
