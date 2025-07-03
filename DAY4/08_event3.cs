c995b73b3bd8aa3fbbba08fd8bee05c510f18b28
ecourse <smkang@codenuri.co.kr>
2025-07-03 15:24:05 +0900

    Add files via upload

---
+++ DAY4/08_event3.cs
@@ -0,0 +1,32 @@
+using static System.Console;
+
+class Button
+{
+    public delegate void Handler();
+
+    public Handler Click = null;
+
+    public void UserPressButton()
+    {
+        if (Click != null)
+        {
+            Click();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Button btn1 = new Button();
+
+        btn1.Click = Foo;
+
+
+        btn1.UserPressButton();
+    }
+    public static void Foo() => WriteLine("Foo");
+    public static void Goo() => WriteLine("Goo");
+    public static void Hoo() => WriteLine("Hoo");
+}
