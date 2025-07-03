3aaa9c0c35b05ec302d1e3a73ed6c5e8dcad7c50
ecourse <smkang@codenuri.co.kr>
2025-07-03 12:10:11 +0900

    Add files via upload

--- DAY4/05_interface0.cs
+++ DAY4/05_interface0.cs
@@ -5,16 +5,21 @@ using static System.Console;
 // 가상 메소드 : 파생 클래스가 override 안해도 됩니다.
 //              override 안하면 기반 클래스가 기본 구현 제공 합니다.
 
-class Shape
+// 추상 메소드 ( abstract method)
+// => 구현이 없고, 이름만 있는 메소드
+// => 반드시 파생 클래스가 override 해서 구현을 제공해야 한다.
+// => 추상 메소드를 만들려면, 클래스 자체도 추상(abstract) 이어야 한다.
+
+abstract class Shape
 {
     public int color = 0;
 
-    public virtual void Draw() { WriteLine("Draw Shape"); }
+    public abstract void Draw();
 }
 
 class Rect : Shape
 {
-//    public override void Draw() { WriteLine("Draw Rect"); }
+    public override void Draw() { WriteLine("Draw Rect"); }
 }
 
 class Program
