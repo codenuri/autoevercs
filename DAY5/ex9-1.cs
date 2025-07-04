8f7722c28ae7c32ac301f665f1b83f7547f82102
ecourse <smkang@codenuri.co.kr>
2025-07-04 11:06:52 +0900

    Add files via upload

--- DAY5/ex9-1.cs
+++ DAY5/ex9-1.cs
@@ -1,17 +1,29 @@
 ﻿// ex8-1
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 
 // 핵심 : UI 는 C# 이 아닌 XML 로 만들자
 
+// 1. ex9-1.xaml 추가
+// 2. ex9-1.xaml 의 속성에서 "빌드 : 없음"
+
 
 class MainWindow : Window
 {
     public MainWindow()
     {
+        FileStream fs = new FileStream("../../../ex9-1.xaml", FileMode.Open);
+
+        Button btn = (Button)XamlReader.Load(fs);
+
+        fs.Close();
+
+        this.Content = btn; 
     }
 }
 
@@ -37,3 +49,4 @@ class App : Application
         app.Run();
     }
 }
+
