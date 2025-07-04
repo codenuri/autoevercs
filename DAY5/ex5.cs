55bdd218c1d6576f8f0a42a35e582a0456bf4c5d
ecourse <smkang@codenuri.co.kr>
2025-07-04 09:29:22 +0900

    Add files via upload

---
+++ DAY5/ex5.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+// 핵심 : Window, Application 클래스를 직접 사용하지 말고
+// => 파생 클래스를 만들어서 사용
+// => Window 객체의 이벤트는 Window 클래스가 처리
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+
+    }
+}
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+}
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new Application();
+
+        Window w = new Window();
+        w.Show();
+
+        app.Run();
+    }
+}
