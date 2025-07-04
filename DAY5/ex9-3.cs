6fb638bc4a59b977a486b36320862d35b34f20a0
ecourse <smkang@codenuri.co.kr>
2025-07-04 11:42:36 +0900

    Add files via upload

---
+++ DAY5/ex9-3.cs
@@ -0,0 +1,44 @@
+﻿// ex8-1
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+
+class MainWindow : Window
+{
+    public void InitializeComponent()
+    {
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+}
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+
+
+        FileStream fs = new FileStream("../../../ex9-3.xaml", FileMode.Open);
+        Window w = (Window)XamlReader.Load(fs);
+
+        w.Show();
+        app.Run();
+    }
+}
+
