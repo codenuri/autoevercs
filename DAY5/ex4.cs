4b6cef0eb2429ee258c9e90d890ede1efed14dc4
ecourse <smkang@codenuri.co.kr>
2025-07-04 09:20:45 +0900

    Add files via upload

---
+++ DAY5/ex4.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+// 핵심 : 윈도우의 다양한 Event 처리하기
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new Application();
+
+        app.Startup += App_Startup;
+        app.Exit += App_Exit;
+
+        Window w = new Window();
+
+        w.MouseRightButtonDown += OnRightDown;
+
+        w.Show();
+
+        app.Run();
+    }
+
+    private static void App_Exit(object sender, ExitEventArgs e)
+    {
+        Console.WriteLine("프로그램이 시작됩니다.");
+    }
+
+    private static void App_Startup(object sender, StartupEventArgs e)
+    {
+        Console.WriteLine("프로그램이 종료됩니다.");
+    }
+
+    private static void OnRightDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("RIGHT DOWN");
+    }
+}
+
