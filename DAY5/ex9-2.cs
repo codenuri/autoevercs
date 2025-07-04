ef83aa10730e6a8a487152038521fea50c8b4124
ecourse <smkang@codenuri.co.kr>
2025-07-04 11:15:21 +0900

    Add files via upload

---
+++ DAY5/ex9-2.cs
@@ -0,0 +1,50 @@
+﻿// ex8-1
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+// ex9-2.xaml 추가 (발음은 자멜, 자믈)
+// 속성 에서 빌드없음
+
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        FileStream fs = new FileStream("../../../ex9-1.xaml", FileMode.Open);
+
+        Button btn = (Button)XamlReader.Load(fs);
+
+        fs.Close();
+
+        this.Content = btn;
+    }
+}
+
+
+
+
+
+
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+        MainWindow w = new MainWindow();
+        w.Show();
+        app.Run();
+    }
+}
+
