e8f9df9795755d83f7a3bf6959091580656491a5
ecourse <smkang@codenuri.co.kr>
2025-07-04 17:02:34 +0900

    Add files via upload

---
+++ DAY5/property.cs
@@ -0,0 +1,21 @@
+﻿// console
+// property.cs
+
+class Point
+{
+    private int x = 0;
+
+    public int X
+    {
+        set { x = value; }
+        get { return x; }   
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Point pt = new Point();
+        pt.X = 10;
+    }
+}
