9573a99902341a8f0fcfd9c6827cc7370b18d033
ecourse <smkang@codenuri.co.kr>
2025-07-04 10:21:09 +0900

    Add files via upload

---
+++ DAY5/ex8-2.cs
@@ -0,0 +1,38 @@
+﻿// ex8-1
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        
+
+    }
+}
+
+
+
+
+
+
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+        MainWindow w = new MainWindow();
+        w.Show();
+        app.Run();
+    }
+}
