b9e4ea7ec010e077b4e65bb97006d2883735fe70
ecourse <smkang@codenuri.co.kr>
2025-07-04 10:45:46 +0900

    Add files via upload

---
+++ DAY5/ex9-1.cs
@@ -0,0 +1,39 @@
+﻿// ex8-1
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+// 핵심 : UI 는 C# 이 아닌 XML 로 만들자
+
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+    }
+}
+
+
+
+
+
+
+
+class App : Application
+{
+    public App()
+    {
+
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+        MainWindow w = new MainWindow();
+        w.Show();
+        app.Run();
+    }
+}
