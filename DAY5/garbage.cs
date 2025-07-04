f21d22dcccc97b99c210489027c2f7c17af84780
ecourse <smkang@codenuri.co.kr>
2025-07-04 17:25:18 +0900

    Add files via upload

---
+++ DAY5/garbage.cs
@@ -0,0 +1,16 @@
+﻿// garbage.cs
+
+class Point { }
+
+// Garbage Collector
+class Program
+{
+    public static void Main()
+    {
+        Point p1;
+        {
+            Point p2 = new Point();
+            int n = 0;
+        }
+    }
+}
