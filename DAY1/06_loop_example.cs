f9aa599b6700c765cfeef915e8d40defafc1f2d8
ecourse <smkang@codenuri.co.kr>
2026-02-20 15:18:25 +0900

    Add files via upload

---
+++ DAY1/06_loop_example.cs
@@ -0,0 +1,13 @@
+﻿// 06_loop_example.cs
+
+// 사용자가 "Q" 를 입력하면 종료, 그렇지 않으면 다시 입력 받아라
+
+// 최소 한번은 입력 받는 루틴이 실행되어야 한다.
+string s;
+
+do
+{
+    Console.Write("input command => ");
+
+    s = Console.ReadLine();
+} while (s != "Q");
