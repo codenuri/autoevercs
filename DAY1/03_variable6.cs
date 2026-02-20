f8f3a871b7b45ef995549fe6a961ec5fd1269ad2
ecourse <smkang@codenuri.co.kr>
2026-02-20 14:19:49 +0900

    Add files via upload

---
+++ DAY1/03_variable6.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int n1 = 10;
+int n2 = 20;
+
+// 변수 값을 화면에 출력하는 방법
+
+// #1. 변수 1개 출력
+WriteLine(n1);
+
+// #2. 2개이상, 또는 서식화된 출력
+WriteLine("n1 = {0}, n2 = {1}", n1, n2);
+
