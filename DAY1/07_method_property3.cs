5ea8846eae38a6106aca195184dac9bb44617944
ecourse <smkang@codenuri.co.kr>
2026-02-20 16:21:08 +0900

    Add files via upload

--- DAY1/07_method_property3.cs
+++ DAY1/07_method_property3.cs
@@ -1,6 +1,6 @@
 using static System.Console;
 
-// 사용자에게 double 값 한개를 입력받아서 화면 출력해 보세요
+// 사용자에게 실수(double) 값 한개를 입력받아서 화면 출력해 보세요
 
 // 1. 문자열 형태로 입력 받을수 밖에 없습니다.
 
@@ -8,5 +8,13 @@ using static System.Console;
 
 // 3. double 변수 출력
 
+string s = Console.ReadLine(); // "3.4"
 
+// "3.4" => 3.4
+// Convert 타입 또는 double 의 static 메소드
 
+double d1 = double.Parse(s);
+double d2 = Convert.ToDouble(s);
+
+Console.WriteLine(d1);
+Console.WriteLine(d2);
