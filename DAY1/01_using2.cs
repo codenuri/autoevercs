d9442866a30666ae223e08777fdca8909b34751b
ecourse <smkang@codenuri.co.kr>
2026-02-20 11:20:31 +0900

    Add files via upload

---
+++ DAY1/01_using2.cs
@@ -0,0 +1,5 @@
+using System; // 현재 파일에만 적용
+global using System;    // 프로젝트내의 모든 소스 코드에서
+                        // System 없이 사용가능
+                        // 반드시 파일의 제일 윗부분에 놓여야 한다
+global using static System.Console;
