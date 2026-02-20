ed17b2b259d28873dfe742757f99ba759f95a411
ecourse <smkang@codenuri.co.kr>
2026-02-20 17:47:09 +0900

    Add files via upload

---
+++ DAY1/wpf5.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Show();
+        w.Title = "AAA";
+
+        w.MouseLeftButtonDown += W_MouseLeftButtonDown;
+        w.MouseRightButtonDown += W_MouseRightButtonDown;
+
+       
+        Application app = new Application();
+        app.Run();
+    }
+
+    private static void W_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Right 버튼 누름");
+    }
+
+    private static void W_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("왼쪽 버튼 누름");
+    }
+}   
