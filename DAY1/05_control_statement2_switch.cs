aad5c9dc6d7eba89c84b94371836e52628a7b4cc
ecourse <smkang@codenuri.co.kr>
2026-02-20 15:03:11 +0900

    Add files via upload

---
+++ DAY1/05_control_statement2_switch.cs
@@ -0,0 +1,25 @@
+using System;
+
+// 핵심 : switch 문
+
+int num = 1;
+
+
+switch(num)
+{
+    case 0:
+        Console.WriteLine("0");
+        break;
+
+    case 1:
+        Console.WriteLine("1");
+        break;
+
+    default:            // 생략 가능
+        Console.WriteLine("other");
+        break;
+}
+
+
+
+
