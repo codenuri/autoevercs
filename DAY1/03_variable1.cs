0c6b615af2f162925dc8b1b95e61c6ed77d34911
ecourse <smkang@codenuri.co.kr>
2026-02-20 12:48:31 +0900

    Add files via upload

--- DAY1/03_variable1.cs
+++ DAY1/03_variable1.cs
@@ -1,3 +1,4 @@
+/*
 // 파이썬 변수 선언시 타입을 표기하지 않는다
 // => 하나의 변수는 모든 종류의 값을 가질수 있다.
 // => dynamic type check 언어(실행시간 타입 조사)
@@ -11,26 +12,27 @@ age = "aaa"  // ok. 의도 일까 ? 실수 일까 ?
 int age = 10;// age 는 정수만 담을수 있다
 age = 20;    // ok
 age = "aaa"; // error
-
-
+//------------------------------------------
+*/
 
 // #1. Data type
 int    n = 0;
 double d = 3.4;
-char   c = 'A';
-string s = "hello";
+char   c = 'A';     // 문자 한개는 싱글 따옴표
+string s = "hello"; // 문자열은 더블 따옴표
 
-// #2. var
-var v1 = 10;		
-var v2 = 3.4;		
-var v3 = "hello";	
+// #2. 타입 위치에 var 사용
+// => 우변의 초기값으로 컴파일러가 타입을 추론해 달라
+var v1 = 10;	    // int v1 = 10	
+var v2 = 3.4;	    // double v2 = 3.4
+var v3 = "hello";	// string v3 = "hello"
 
 // #3. literal
-int a1 = 10;		
-int a2 = 0x10;		
-int a3 = 0b10;		
+int a1 = 10;		// 10진수
+int a2 = 0x10;		// 16진수
+int a3 = 0b10;		// 2진수, a3 는 결국 2
 
 int a4 = 1000000;	
-int a5 = 1_000_000; 
+int a5 = 1_000_000; // 값이 큰경우 _ 로 자리표시 가능
 
 
