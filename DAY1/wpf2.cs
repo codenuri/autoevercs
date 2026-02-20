6eabeeb63146bf586ca524bb2300ce0760b823c9
ecourse <smkang@codenuri.co.kr>
2026-02-20 17:23:22 +0900

    Add files via upload

---
+++ DAY1/wpf2.cs
@@ -0,0 +1,19 @@
+﻿// wpf2.cs
+// 핵심 : 직접 Window 만들기
+// => 지금 단계 부터는 Top-Level 안됩니다.
+// => Main 메소드 만드는 방식
+
+using System.Windows;
+
+class Program
+{
+    // GUI 를 만든경우 Main 위에 반드시 아래 한줄 필요(관례적인 코드)
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Show();
+
+        MessageBox.Show("Hello");
+    }
+}
