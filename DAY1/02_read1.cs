18a0f0fed85a5ec6e856a77354cb9a34da51da0d
ecourse <smkang@codenuri.co.kr>
2026-02-20 11:36:03 +0900

    Add files via upload

--- DAY1/02_read1.cs
+++ DAY1/02_read1.cs
@@ -2,12 +2,14 @@
 
 Console.Write("input your name >> ");
 
-// Console 클래스 : 입출력 관련 다양한 메소드 제공
-string s = Console.ReadLine();  
+// Console 클래스 : 입출력 관련 다양한 메소드(함수) 제공
+string s = Console.ReadLine();  // 사용자 입력 대기
 
-Console.WriteLine(s);   
- 
-      
+Console.WriteLine(s);
+
+// Console 관련 모든 기능을 "Console" 클래스의 메소드에서 찾으면됩니다.
+Console.Clear();
+Console.WriteLine("ABC");
   
   
    
