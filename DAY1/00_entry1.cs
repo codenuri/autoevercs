882149422c8047a21fcb0483ac0b21c1e625ebb6
ecourse <smkang@codenuri.co.kr>
2026-02-20 11:07:46 +0900

    Add files via upload

---
+++ DAY1/00_entry1.cs
@@ -0,0 +1,23 @@
+﻿// C# 언어
+// 소스 코드 확장자 : .cs    
+// 파일 포맷        : UTF-8 권장, 다른 포맷도 가능
+// 소스 스타일 : C/C++ 과 유사, 문장의 끝에는 반드시 세미콜론(;) 필요
+//              대소문자 구별됨
+
+// entry point 는 2가지 방식 제공
+// 1. 클래스를 한개 만들고 Main 이라는 static 메소드 제공
+//    (클래스, static 의 의미를 3일차 배우게 됩니다. 지금은 외우세요)
+// => C#1.0 부터 사용되던 전통적인 방식
+
+// 2. Top-Level Programming 방식(파이썬과 동일)
+// => C#9.0 부터 지원
+// => 소스 파일의 1번째 문장 부터 실행
+
+// 아래 코드는 1번 방식
+class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("hello, C#");
+    }
+}
---
+++ DAY1/00_entry3.cs
@@ -0,0 +1,14 @@
+﻿// Top-Level 방식은 제약 조건이 많습니다.
+// => class 라는 것은 반드시 소스에 아래 부분에 있어야 한다
+
+//class Car { }  // error.
+
+System.Console.WriteLine("hello, C#");
+
+class Car { }
+
+// 수업방식
+// 간단한 예제 : Top-Level 방식 사용
+// 중요(복잡한)예제 : Main 함수 방식
+
+// 현업 : 모두 Main함수 방식
