5adf9dc32486d46063c3cb43d9de873f92b6bc26
ecourse <smkang@codenuri.co.kr>
2026-02-20 17:07:09 +0900

    Add files via upload

---
+++ DAY1/wpf1.cs
@@ -0,0 +1,18 @@
+﻿
+
+// C# 언어의 장점
+// => 강력한 문법 + 풍부한 라이브러리(분야별 수천~수만개의 미리 만들어진 타입(클래스))
+
+// WPF(Windows Presentation Framework)
+// => GUI 용 라이브러리
+// => Direct-X 엔진을 사용한 강력하고 유연한 GUI 작성 가능
+
+
+Console.WriteLine("Hello, World!");
+
+
+// System.Windows.MessageBox 클래스를 사용하면 MessageBox를 나타낼수 있습니다.
+MessageBox.Show("Hello");
+
+
+// 파일이름 wpf1.cs로 변경
