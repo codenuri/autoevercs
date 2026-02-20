2ae23118818a739979d67bbd640c87ee139d3ca6
ecourse <smkang@codenuri.co.kr>
2026-02-20 17:38:12 +0900

    Add files via upload

---
+++ DAY1/wpf4.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+// 핵심 
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Show();
+
+        // 윈도우 캡션 문자열을 "AAA"로 변경해 보세요
+        // => 현재 윈도우를 관리하는 객체(변수)는 w
+        // => 윈도우에 대해서 어떤 작업을 하고 싶다면
+        // => "w." 후에 lookup table 에서 찾아라
+
+        w.Title = "AAA";
+        w.Width = 300;
+        w.Height = 300;
+
+//        w.Background = new SolidBrush(Colors.Yellow);
+
+        Application app = new Application();
+        app.Run();
+    }
+}
