b1a253c9015cd4d42be448d08d24b61ff18156e6
ecourse <smkang@codenuri.co.kr>
2026-02-23 11:32:59 +0900

    Add files via upload

---
+++ DAY2/06_property3.cs
@@ -0,0 +1,23 @@
+class Person
+{
+    private int age;
+
+
+    // get/set 중 한개만 만들어도 됩니다.
+    public int Age
+    {
+//      get { return age; }
+        set { if (value > 0) age = value; }
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+
+        p.Age = 10;     // ok
+//      int n = p.Age;  // error
+
+    }
+}
