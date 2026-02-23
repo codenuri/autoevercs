d71e2317be214318faa8ac50add9a7a80d1ba017
ecourse <smkang@codenuri.co.kr>
2026-02-23 14:17:18 +0900

    Add files via upload

--- DAY2/07_static5.cs
+++ DAY2/07_static5.cs
@@ -9,6 +9,14 @@ class Date
     public int Day   { get; set; };
 
     public Date(int y, int m, int d) => (Year, Month, Day) = (y, m, d);
+
+    // Date 타입 사용자가 유용하게 사용할수 있는 다양한 메소드를 제공해야 합니다.
+
+    public Date AfterDays(int ds)
+    {
+        Date tmp = new Date(Year, Month, Day + ds); // ds 이후의 날짜
+        return tmp;
+    }
 }
 
 class Program
@@ -16,6 +24,8 @@ class Program
     public static void Main()
     {
         Date d1 = new Date(2025, 2, 23);
+
+        Date d2 = d1.AfterDays(1000);
 
     }
 }
