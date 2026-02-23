7260ffb70cfca9ea5150ef61342501c90de4d01a
ecourse <smkang@codenuri.co.kr>
2026-02-23 17:02:32 +0900

    Add files via upload

---
+++ DAY2/10_upcasting1.cs
@@ -0,0 +1,28 @@
+﻿// upcasting 개념 : 객체지향 언어에서 가장 중요한 개념중 하나!!
+class Animal
+{
+    public int Age { set; get; } = 0;
+}
+
+class Dog : Animal
+{
+    public int Color { set; get; } = 0;
+}
+
+class Cat : Animal
+{
+    public int Speed { set; get; } = 0;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Dog r1 = new Dog(); // ?
+
+        int r2 = new Dog(); // ?
+
+        Animal r3 = new Dog(); // ?
+
+    }
+}
