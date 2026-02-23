569b46027bda0523b6fc2de351bd30db638eb2d7
ecourse <smkang@codenuri.co.kr>
2026-02-23 14:37:06 +0900

    Add files via upload

---
+++ DAY2/07_static7.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+
+class Date
+{
+    // 강의에서는 간단히 하기 위해 auto-impl 사용
+    // 복습시에는 유효성 여부를 검증하도록 변경해 보세요
+    public int Year { get; set; };
+    public int Month { get; set; };
+    public int Day { get; set; };
+
+    private static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+
+    public Date(int y, int m, int d) => (Year, Month, Day) = (y, m, d);
+
+
+    public Date AfterDays(int ds)
+    {
+        Date tmp = new Date(Year, Month, Day + ds);
+
+        return tmp;
+    }
+
+    public static int HowManyDays(int m)
+    {
+        return days[m - 1];
+    }
+
+    public Date Tomorrow()
+    {
+        Date tmp = new Date(Year, Month, Day + 1); 
+        return tmp;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        // 윤년인지 조사하는 IsLeapYear() 메소드를 제공하고싶다.
+
+        // 다음중 좋은 방법은 ?
+        // #1. instance method 로 제공
+        Date d  = new Date(2025, 2, 23);
+        bool b1 = d.IsLeapYear();
+
+        // #2. static method 로 제공
+        bool b2 = Date.IsLeapYear(2025);
+
+    }
+}
