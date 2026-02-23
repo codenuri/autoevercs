aca9b56b3fc7c5ea582b46b9d697662849856791
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:22:22 +0900

    Add files via upload

---
+++ DAY2/07_static1.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+// static 개념
+// => 모든 (파이썬 포함) 객체지향 언어의 핵심!!! 
+// => 반드시 이해해야 합니다.
+
+// Car 객체(자동차)를 몇대나 만들었는지 알고 싶다
+
+
+class Car 
+{
+    private int speed = 0;
+    private int color = 10;
+
+    public Car(int s)
+    {
+        speed = s;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car(50);
+        Car c2 = new Car(80);
+
+    }
+}
