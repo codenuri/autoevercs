48f42c630a31cc583edd6a4aab323466bf3b9ed4
ecourse <smkang@codenuri.co.kr>
2026-02-23 14:14:26 +0900

    Add files via upload

---
+++ DAY2/07_static5.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+class Date
+{
+    // 강의에서는 간단히 하기 위해 auto-impl 사용
+    // 복습시에는 유효성 여부를 검증하도록 변경해 보세요
+    public int Year  { get; set; };
+    public int Month { get; set; };
+    public int Day   { get; set; };
+
+    public Date(int y, int m, int d) => (Year, Month, Day) = (y, m, d);
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Date d1 = new Date(2025, 2, 23);
+
+    }
+}
