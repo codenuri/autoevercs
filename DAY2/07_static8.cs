48920bfeb0f5f5b548130f4993bb388c7008b13d
ecourse <smkang@codenuri.co.kr>
2026-02-23 14:43:59 +0900

    Add files via upload

---
+++ DAY2/07_static8.cs
@@ -0,0 +1,23 @@
+﻿// 다음 코드에서 에러를 모두 찾으세요
+class Car
+{
+    private int speed = 0;
+    private static int cnt = 0;
+
+    public static void F1()
+    {
+        speed = 10; // ?
+        cnt = 10;   // ?
+    }
+    public void F2()
+    {
+        speed = 10; // ?
+        cnt = 10;   // ?
+    }
+}
+class Program
+{
+    public static void Main()
+    { 
+    }
+}
