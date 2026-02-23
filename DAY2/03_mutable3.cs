90f4bf4659b532dbf37e6ed33de3bb9d910ced72
ecourse <smkang@codenuri.co.kr>
2026-02-23 16:22:33 +0900

    Add files via upload

---
+++ DAY2/03_mutable3.cs
@@ -0,0 +1,14 @@
+﻿// java, C#, Swift 에서
+// 문자열 타입은 2개가 있습니다.
+// 1. immutable string
+// 2. mutable string
+
+using System.Text;
+
+string s1 = "ABC"; // immutable, 절대 변경할수 없음
+
+StringBuilder s2 = new StringBuilder("XYZ"); // mutable 변경가능
+                                             // 단축표기법 없음
+                                             // new 만 가능
+//s1[0] = 'X'; // error. immutable
+s2[0] = 'X'; // ok
