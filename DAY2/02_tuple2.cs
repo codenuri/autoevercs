bd6f0557a875480e971bb95119a23abb153dc935
ecourse <smkang@codenuri.co.kr>
2026-02-23 09:36:13 +0900

    Add files via upload

--- DAY2/02_tuple2.cs
+++ DAY2/02_tuple2.cs
@@ -17,12 +17,21 @@ var t3 = (1, 2, 3);
 
 t3.Item1 = 10;
 
-Console.WriteLine("{}, {}", t3.Item1, t3.Item2)
+Console.WriteLine("{0}, {1}", t3.Item1, t3.Item2);
 
 
 
 
 // #3. Named Member
-(int, int, int) v3 = (1, 2, 3);
+(int, int, int) v3               = (1, 2, 3); // unnamed tuple
+(int one, int two, int three) v4 = (1, 2, 3); // named tuple
 
+Console.WriteLine("{0}, {1}", v3.Item1, v3.Item2);
+Console.WriteLine("{0}, {1}", v4.one,   v4.two);
 
+// #4. Named member 를 만드는 방법은 2가지
+// => 1. 타입에 표기
+// => 2. 초기값에 표기
+(int one, int two, int three) v5 = (1, 2, 3);
+(int, int, int) v6 = (one:1, two:2, three:3);
+var v7             = (one:1, two:2, three:3);
