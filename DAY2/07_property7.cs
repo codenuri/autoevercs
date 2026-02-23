79018f2777bd233c74724f353aad1301ddd11390
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:20:20 +0900

    Add files via upload

--- DAY2/07_property7.cs
+++ DAY2/07_property7.cs
@@ -11,11 +11,22 @@
         get => age;
         set {  if (value > 0) age = value; }    
     }
+    // 핵심
+    // Person1 의 구현이 변경되었지만
+    // => 사용자 코드는 변경될 필요 없다
 }
+
 class Person2
 {
     // public 필드 : 값의 유효성 확인 없음
     public int age = 0;
+
+    // age 부분을 property 로 만들게 되면
+    // 외부 사용자 코드도 모두 변경되어야 한다.
+
+    // 즉, 유효성 검사가 당장 없더라도..
+    // => public 필드로 하지 말고
+    // => auto property 로 만드는 것이 좋다
 }
 
 class Program
