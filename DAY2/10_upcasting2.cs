a06c814c59915d2ec417a0249e556fc78f63a75b
ecourse <smkang@codenuri.co.kr>
2026-02-23 17:33:32 +0900

    Add files via upload

--- DAY2/10_upcasting2.cs
+++ DAY2/10_upcasting2.cs
@@ -29,11 +29,30 @@ class Program
             d.Color = 10;
         }
     }
+    // 그럼, Object 로 받으면 모든 객체를 받을수 있지 않나요 ?
+    public static void Foo(object obj)
+    {
+        // 하지만 이경우 Age 접근하려면 캐스팅 필요
+    }
 
     public static void Main()
     {
         NewYear(new Dog());
         NewYear(new Cat());
+        NewYear(10); // error
+
+        Foo(new Dog()); // ok
+        Foo(new Cat()); // ok
+        Foo(10); // ok. 
+
+        Dog[] arr1 = new Dog[10]; // Dog 객체 10개 보관 배열
+        arr1[0] = new Dog();
+        arr1[1] = new Cat();  // error
+
+        Animal[] arr2 = new Animal[10]; // 모든 동물을 보관하는 배열
+                                        // 동종을 보관하는 컬렉션
+        arr2[0] = new Dog(); // ok
+        arr2[1] = new Cat(); // ok
 
     }
 }
