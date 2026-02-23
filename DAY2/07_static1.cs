d617ed522e1a5971dcdd8985cc65863991929d3d
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:25:00 +0900

    Add files via upload

--- DAY2/07_static1.cs
+++ DAY2/07_static1.cs
@@ -6,15 +6,22 @@
 
 // Car 객체(자동차)를 몇대나 만들었는지 알고 싶다
 
+// 방법 #1. 모든 객체는 만들때 생성자가 호출된다.
+// => cnt 라는 필드를 만들고
+// => 생성자에서 ++ 하자
 
 class Car 
 {
     private int speed = 0;
     private int color = 10;
 
+    public int cnt = 0;
+
     public Car(int s)
     {
         speed = s;
+
+        ++cnt;
     }
 }
 
@@ -24,6 +31,8 @@ class Program
     {
         Car c1 = new Car(50);
         Car c2 = new Car(80);
+
+        Console.WriteLine($"{c1.cnt}"); // ???
 
     }
 }
