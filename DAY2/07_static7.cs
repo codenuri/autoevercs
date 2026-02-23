3a21ae4c5c106d91a62aba913bbb581de1ff04b2
ecourse <smkang@codenuri.co.kr>
2026-02-23 14:41:43 +0900

    Add files via upload

--- DAY2/07_static7.cs
+++ DAY2/07_static7.cs
@@ -31,6 +31,19 @@ class Date
         Date tmp = new Date(Year, Month, Day + 1); 
         return tmp;
     }
+
+    // 동일 기능을 instance method, static method 로 모두제공
+    // 하는 것도 많이 사용되는 기술입니다.
+    public static bool IsLeapYear(int y)
+    {
+        return (y % 400 == 0) || ( (y % 4 == 0) && (y % 100 != 0) ); 
+    }
+
+    public bool IsLeapYear()
+    {
+        // static method 호출하면서 자신의 Year 전달
+        return IsLeapYear(Year);
+    }
 }
 
 class Program
@@ -41,11 +54,11 @@ class Program
 
         // 다음중 좋은 방법은 ?
         // #1. instance method 로 제공
-        Date d  = new Date(2025, 2, 23);
+        Date d  = new Date(2026, 2, 23);
         bool b1 = d.IsLeapYear();
 
         // #2. static method 로 제공
-        bool b2 = Date.IsLeapYear(2025);
+        bool b2 = Date.IsLeapYear(2026);
 
     }
 }
