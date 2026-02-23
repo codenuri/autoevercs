76613387c430081f167e0cd8a5beb95334210264
ecourse <smkang@codenuri.co.kr>
2026-02-23 16:17:58 +0900

    Add files via upload

--- DAY2/03_mutable2.cs
+++ DAY2/03_mutable2.cs
@@ -6,7 +6,7 @@ s2 = "XY"; // new string("XY") 의 의미
            // 이순간 메모리 그림 생각
 
 Console.WriteLine($"{s1} {s2}");
-
+/*
 class CPoint
 {
     public int x;
@@ -18,4 +18,6 @@ CPoint pt = new CPoint(0, 0);
 pt.x = 10; // pt 가 가리키는 힙객체의 속성(필드) 변경한것
 
 pt = new CPoint(1, 1);// 힙에 새로운 객체 만든것
+
 s2 = "XY"; // 이 코드가 위 코드 의미.
+*/
