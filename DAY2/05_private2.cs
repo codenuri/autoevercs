fcbb72a147320439920755bd91222402535c373e
ecourse <smkang@codenuri.co.kr>
2026-02-23 11:07:00 +0900

    Add files via upload

---
+++ DAY2/05_private2.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+class Bike
+{
+    private int gear = 0;
+
+    public void SetGear(int g)
+    {
+        gear = g; 
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Bike b = new Bike();
+
+//      b.gear = -10; // error
+        b.SetGear(-10); // ok
+    }
+}
+
+
