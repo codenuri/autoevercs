ae71344bf0d350eba293f7c5765c0b44d75f8f63
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:16:49 +0900

    Add files via upload

--- DAY2/07_property7.cs
+++ DAY2/07_property7.cs
@@ -1,7 +1,16 @@
 ﻿class Person1
 {
     // 자동 구현 property : 값의 유효성 확인 없음
-    public int Age { get; set; } = 0;
+//  public int Age { get; set; } = 0;
+
+    // 미래에 위 코드가 유효성 확인을 위해 아래 처럼 변경
+    public int age = 0;
+
+    public int Age
+    {
+        get => age;
+        set {  if (value > 0) age = value; }    
+    }
 }
 class Person2
 {
@@ -16,7 +25,7 @@ class Program
         Person1 p1 = new Person1();
         Person2 p2 = new Person2();
 
-        p1.Age = 10;
-        p2.age = 10;
+        p1.Age = -10;
+        p2.age = -10;
     }
 }
