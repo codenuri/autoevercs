ce0f7b535777aae9930cdb02753d7479f7cbee1d
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:13:43 +0900

    Add files via upload

---
+++ DAY2/07_property7.cs
@@ -0,0 +1,22 @@
+﻿class Person1
+{
+    // 자동 구현 property : 값의 유효성 확인 없음
+    public int Age { get; set; } = 0;
+}
+class Person2
+{
+    // public 필드 : 값의 유효성 확인 없음
+    public int age = 0;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Person1 p1 = new Person1();
+        Person2 p2 = new Person2();
+
+        p1.Age = 10;
+        p2.age = 10;
+    }
+}
