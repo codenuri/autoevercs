cd5f4b70ec11345f13b2924cad8124e2006ca133
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:28:36 +0900

    Add files via upload

--- DAY2/07_static1.cs
+++ DAY2/07_static1.cs
@@ -6,7 +6,8 @@
 
 // Car 객체(자동차)를 몇대나 만들었는지 알고 싶다
 
-// 방법 #1. 모든 객체는 만들때 생성자가 호출된다.
+// 방법 #1. 일반 필드 cnt 사용
+// => 모든 객체는 만들때 생성자가 호출된다.
 // => cnt 라는 필드를 만들고
 // => 생성자에서 ++ 하자
 
@@ -15,12 +16,12 @@ class Car
     private int speed = 0;
     private int color = 10;
 
-    public int cnt = 0;
+    public int cnt = 0; // 이렇게 만들면 모든 자동차 객체에 대해서 각각 
+                        // 메모리에 놓이게 됩니다.
 
     public Car(int s)
     {
         speed = s;
-
         ++cnt;
     }
 }
