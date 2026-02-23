5a349042bf9d3768c557f7614e726736aa57d336
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:38:12 +0900

    Add files via upload

--- DAY2/07_static2.cs
+++ DAY2/07_static2.cs
@@ -5,7 +5,7 @@
 class Car
 {
     private int speed = 0;
-    private int color = 10;
+    private int color = 10; // instance field
 
 //  public int cnt = 0; 
     public static int cnt = 0;  // static field
@@ -27,10 +27,22 @@ class Program
 {
     public static void Main()
     {
+        // Car 객체가 한개도 없어도
+        // static field 는 메모리에 놓이게 됩니다.
+        // public 에 있다면 "Car.cnt" 이름으로 언제라도 접근 가능
+        Console.WriteLine($"{Car.cnt}"); // 0
+
         Car c1 = new Car(50);
         Car c2 = new Car(80);
 
-        Console.WriteLine($"{Car.cnt}"); // ???
+        Console.WriteLine($"{Car.cnt}"); // 2
 
     }
 }
+
+// 핵심
+// instance field : 객체 각각의 대한 특징을 나타내는 데이타
+//                  => 자동차의 속도(speed) 는 각각의 자동차가 다르다.
+
+// static field : 타입 자체에 대한 특징. 동일 타입의 모든 객체가 공유
+//                  => 자동차의 갯수등
