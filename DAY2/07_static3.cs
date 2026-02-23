e144fdf0498189df45919b79fb8ef84558b21ebc
ecourse <smkang@codenuri.co.kr>
2026-02-23 12:45:16 +0900

    Add files via upload

--- DAY2/07_static3.cs
+++ DAY2/07_static3.cs
@@ -9,8 +9,13 @@ class Car
 
     private static int cnt = 0;   // 이제 외부에서 직접 접근 못함
 
-    public int GetCount() {  return cnt; } 
+//  public int GetCount() {  return cnt; }  // instance method
+                                        // "객체.메소드()"로 호출
+                                        // 반드시 객체가 있어야 호출가능
 
+    public static int GetCount() { return cnt; }  // static method
+                                        // 객체 없이 호출가능한 메소드
+                                        // "클래스이름.메소드()"로 호출
 
     public Car(int s)
     {
@@ -23,14 +28,14 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine($"{Car.cnt}");
+        Console.WriteLine($"{Car.GetCount()}");
 
 //        Car.cnt = 100;
 
         Car c1 = new Car(50);
         Car c2 = new Car(80);
 
-        Console.WriteLine($"{Car.cnt}"); 
+        Console.WriteLine($"{Car.GetCount()}");  // 2
 
     }
 }
