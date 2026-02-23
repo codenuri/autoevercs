3c2442e32a33786df17af45366f77d14eba03b3d
ecourse <smkang@codenuri.co.kr>
2026-02-23 17:36:39 +0900

    Add files via upload

--- DAY2/wpf2.cs
+++ DAY2/wpf2.cs
@@ -1,21 +1,10 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-class Program
+namespace DAY2Example
 {
-    [STAThread]
-    public static void Main()
+    internal class wpf33
     {
-        Window w = new Window { Title="Hello", Width=300, Height=300 };
-
-        Button b = new Button { Content = "확인", FontSize = 120 };
-
-        w.Content = b;
-
-        w.Show();
-
-        // UI 를 만든경우, 종료되지 말고 event loop 실행
-        Application app = new Application();
-        app.Run();
     }
 }
--- DAY2/wpf3.cs
+++ DAY2/wpf3.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows;
+using System.Windows.Controls;
 
-namespace DAY2Example
+class Program
 {
-    internal class wpf33
+    [STAThread]
+    public static void Main()
     {
+        Window w = new Window();
+        
+        w.Title = "Hello"; // 윈도우의 캡션 문자열에 나타낼 문자열 설정
+
+        // Content 속성 : 캡션바가 아닌 Window 자체에 나타낼 "컨텐츠" 연결
+        w.Content = "Hello";
+
+
+
+
+        w.Show();
+
+        
+        Application app = new Application();
+        app.Run();
     }
 }
