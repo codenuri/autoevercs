99871dea0acf5ab06d452e6ede170260dacbcd24
ecourse <smkang@codenuri.co.kr>
2026-02-24 12:08:48 +0900

    Add files via upload

---
+++ DAY3/wpf1.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Show();
+
+        Application app = new Application();
+        app.Run();
+    }
+}
