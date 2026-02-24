2aa5f004313d624ac28d27709fe78172466efdf8
ecourse <smkang@codenuri.co.kr>
2026-02-24 10:17:38 +0900

    Add files via upload

---
+++ DAY3/03_example2.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+// 핵심 #2.
+
+class Rect
+{
+    public void Draw() { WriteLine("Draw Rect"); }
+}
+
+class Circle
+{
+    public void Draw() { WriteLine("Draw Circle"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            int cmd = int.Parse(Console.ReadLine());
+
+            if (cmd == 1)
+            {
+                Rect r = new Rect();
+            }
+            else if (cmd == 2)
+            {
+                Circle c = new Circle();
+            }
+        }
+    }
+}
