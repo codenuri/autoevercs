d95f8dcae7156aa1682b5774ae656cf137d5c05a
ecourse <smkang@codenuri.co.kr>
2026-02-24 11:02:38 +0900

    Add files via upload

--- DAY3/03_example5.cs
+++ DAY3/03_example5.cs
@@ -5,23 +5,27 @@
 // => 모든 도형이 Draw()가 있다면  Shape 에도 Draw() 가 있어야 
 // => Shape 참조로 Draw() 호출 가능
 
+// 핵심 #6. 기반 클래스 메소드중 파생 클래스가 다시 만들게 되는 것은
+// => 대부분 virtual method 로 해야 한다
+// => 그래야, Shape 참조로 호출시 Rect, Circle 에서 다시 만든 Draw() 호출
+
 class Shape
 {
     private int color = 0;
 
-    public void Draw() { WriteLine("Draw Shape"); }
+    public virtual void Draw() { WriteLine("Draw Shape"); }
 }
 
 
 
 class Rect : Shape
 {
-    public void Draw() { WriteLine("Draw Rect"); }
+    public override void Draw() { WriteLine("Draw Rect"); }
 }
 
 class Circle : Shape
 {
-    public void Draw() { WriteLine("Draw Circle"); }
+    public override void Draw() { WriteLine("Draw Circle"); }
 }
 
 class Program
