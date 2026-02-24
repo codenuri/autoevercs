f72e39476ebc4e156a6a7f28e08f84762acc01f5
ecourse <smkang@codenuri.co.kr>
2026-02-24 14:28:49 +0900

    Add files via upload

---
+++ DAY3/panel4.cs
@@ -0,0 +1,46 @@
+﻿// panel1.cs
+using System.Windows;
+using System.Windows.Controls;
+
+// 프로그램을 배우는 것은 ?
+// 1. 프로그래밍 언어(C#, C++, Java, Python 등) 문법
+// 2. 해당 언어로 만들어진 라이브러리 사용법
+
+// 라이브러리 사용시 대부분 지켜야 하는 규칙들이 있습니다.
+// => Framework 라고 합니다. - 각 라이브러리는 자신만의 규칙이 있습니다.
+// => WPF : Window Presentation Framework
+
+// WPF 규칙중 한개
+// => 윈도우에 여러개 자식컨트롤을 붙이려면 Panel 을 사용해야 한다.
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        DockPanel dp = new DockPanel();
+        Content = dp;
+
+        Button b1 = new Button { Content = "OK" };
+        TextBox tb = new TextBox();
+
+        DockPanel.SetDock(tb, Dock.Top);
+        DockPanel.SetDock(b1, Dock.Bottom);
+
+
+        dp.Children.Add(tb);
+        dp.Children.Add(b1);
+    }
+}
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainWindow w = new MainWindow();
+        w.Show();
+
+        Application app = new Application();
+        app.Run();
+    }
+}
