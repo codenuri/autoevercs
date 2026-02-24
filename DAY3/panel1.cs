3b7d916d1897a3ce20325abf58f68cb01c75ffe1
ecourse <smkang@codenuri.co.kr>
2026-02-24 14:09:33 +0900

    Add files via upload

---
+++ DAY3/panel1.cs
@@ -0,0 +1,19 @@
+﻿// panel1.cs
+using System.Windows;
+
+class MainWindow : Window
+{
+}
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainWindow w = new MainWindow();
+        w.Show();
+
+        Application app = new Application();
+        app.Run();  
+    }
+}
