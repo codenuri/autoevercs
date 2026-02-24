1b834a79aebafec6ce4d7dcba73569c01e26d762
ecourse <smkang@codenuri.co.kr>
2026-02-24 11:29:46 +0900

    Add files via upload

--- DAY3/03_example7.cs
+++ DAY3/03_example7.cs
@@ -2,9 +2,28 @@
 
 // 핵심 #8. virtual vs non-virtual 
 
+// Shape 메소드 중에서
+// => 파생 클래스가 다시 만들게 되면 : virtual method 로
+// => 파생 클래스가 다시 만들필요 없으면 : non-virtual 로
+//                                      virtual 로 해도 되지만
+//                                      (virtual 은 오버헤드가 있습니다.)
+
+
 class Shape
 {
     private int color = 0;
+
+    // 아래 메소드는 virtual 로 할까요 ? non-virtual 로 할까요 ?
+    // 아래 메소드는 파생 클래스가 다시 만들이유가 없습니다.
+    // => non-virtual
+    // => virtual 로 해도 문제 없지만, virtual 은 성능 저하가 있습니다.
+    public void SetColor(int c) {  color = c; }
+
+    // 도형의 면적을 구하는 방법은 "각 도형" 마다 다릅니다.
+    // => 즉, 아래 메소드는 파생 클래스가 override 하게 되고
+    // => 이때, override 된 메소드가 불리게 하려면 virtual
+   public virtual int GetArea() { return 0; }
+
 
     public virtual void Draw() { WriteLine("Draw Shape"); }
         
