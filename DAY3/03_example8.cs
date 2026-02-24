ab84031b3c051120560eff44b690545cb95efd7f
ecourse <smkang@codenuri.co.kr>
2026-02-24 11:39:52 +0900

    Add files via upload

--- DAY3/03_example8.cs
+++ DAY3/03_example8.cs
@@ -2,7 +2,9 @@
 
 // 핵심 #9. abstract
 
-class Shape
+// abstract method 가 한개 이상 있다면
+// 클래스 자체도 abstract 가 되어야 합니다.
+abstract class Shape
 {
     private int color = 0;
 
@@ -10,16 +12,23 @@ class Shape
 
 
 
+    // 가상 메소드 : 기반 클래스가 기본 구현 제공,
+    //              파생 클래스는 기본 구현 사용하거나, 다시 만들수 있다
+    
+    // 추상 메소드 : 기반 클래스가 구현없이 이름만 약속하는 것
+    //              파생 클래스는 반드시 구현해야 한다
 
-    public virtual void Draw() { WriteLine("Draw Shape"); }
+//  public virtual void Draw() { WriteLine("Draw Shape"); }
 
-    public virtual Shape Clone()
-    {
-        Shape s = new Shape();
-        s.color = color;
-        return s;
-    }
-    public virtual int GetArea() { return 0; }
+    public abstract void Draw();
+    public abstract Shape Clone();
+    
+    // 추상 메소드 : 파생 클래스가 반드시 만들어야 한다.
+    
+    // 아래 메소드도 추상으로 하면 파생 클래스가 반드시 만들어야 합니다.
+    // 혹시 "다시 만드는 것을 옵션" 으로 하려면 아래 처럼 virtual 로 하고
+    // 기본(면적 구할수 없음을 나타내는 ) 값  반환
+    public virtual int GetArea() { return -1; }
 }
 
 
