aca59deda31df3f7899b8fa3463a792bca0043e2
ecourse <smkang@codenuri.co.kr>
2026-02-24 10:59:37 +0900

    Add files via upload

--- DAY3/03_example5.cs
+++ DAY3/03_example5.cs
@@ -1,10 +1,15 @@
 ﻿using static System.Console;
 
-// 핵심 #5. 
+// 핵심 #5. 모든 도형의 공통의 특징은 반드시 기반 클래스에도 있어야 한다.
+// => 문법의 규칙이 아닌 "디자인의 규칙"
+// => 모든 도형이 Draw()가 있다면  Shape 에도 Draw() 가 있어야 
+// => Shape 참조로 Draw() 호출 가능
 
 class Shape
 {
     private int color = 0;
+
+    public void Draw() { WriteLine("Draw Shape"); }
 }
 
 
