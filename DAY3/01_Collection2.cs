371c0c1133dc9355d3a415f906a6b9a01c909942
ecourse <smkang@codenuri.co.kr>
2026-02-24 09:20:38 +0900

    Add files via upload

---
+++ DAY3/01_Collection2.cs
@@ -0,0 +1,16 @@
+﻿// Collection 
+class Program
+{
+    public static void Main()
+    {        
+        List<int> s1 = new List<int>(); // 정확한 표기법
+
+        List<int> s2 = new(); // 단축 표기법
+
+        // C# 은 컴파일러의 의한 타입 추론이 아주 강력합니다.
+        // s2 처럼만 표기해도 s1 과 완전히 동일
+
+
+    }
+}
+
