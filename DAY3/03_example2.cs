43ac10007e458414a3d5ccdbb8f3daeaeaa96577
ecourse <smkang@codenuri.co.kr>
2026-02-24 10:22:43 +0900

    Add files via upload

--- DAY3/03_example2.cs
+++ DAY3/03_example2.cs
@@ -1,6 +1,20 @@
 ﻿using static System.Console;
 
-// 핵심 #2.
+// 핵심 #2. 
+
+// 모든 도형은 공통의 특징이 있습니다.
+// => color 같은 특징..
+
+// 공통의 기반 클래스인 Shape 가 있다면
+// => 공통의 특징을 하나의 클래스(Shape) 에서 관리할수 있고
+// => 모든 도형을 한개의 컬렉션(List<Shape>) 에 보관할수 있습니다.
+
+class Shape
+{
+    private int color = 0;
+}
+
+
 
 class Rect
 {
@@ -16,6 +30,9 @@ class Program
 {
     public static void Main()
     {
+        // 만들어지는 모든 도형을 보관하기 위한 컬렉션
+        List<Rect> s = new List<Rect>();
+
         while (true)
         {
             int cmd = int.Parse(Console.ReadLine());
