f9ead367031740864721acc388e0fe6390b728c3
ecourse <smkang@codenuri.co.kr>
2026-02-24 15:10:16 +0900

    Add files via upload

--- DAY3/puzzle1.cs
+++ DAY3/puzzle1.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
 using System.Windows.Media.Imaging;
+
 
 // puzzle Step #1. 그림 Load해서 윈도우에 연결
 
@@ -8,13 +10,19 @@ class MainWindow : Window
     public MainWindow() 
     {
         // #1. 자원의 경로를 관리하는 객체 생성
+        // => Uniform Resource Identity
         Uri uri = new Uri("C:\\totoro.jpg"); // 그림 경로 
 
         // #2. 그림(Bitmap) 을 Load 하는 객체 생성
         BitmapImage bm = new BitmapImage(uri);
 
 
-        // #4. Content 에 연결
+        // #3. Load 된 그림을 Content 로 연결 또는 panel 의 자식으로 넣으려면
+        // => Image 타입의 객체가 필요
+        Image img = new Image { Source =  bm };
+
+
+        // #4. Content 에 Imae 객체 연결
         Content = bm;
     
     }
