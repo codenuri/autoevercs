f38da205c36979f2a329587a9b529ffbccf4e9e8
ecourse <smkang@codenuri.co.kr>
2026-02-24 09:29:17 +0900

    Add files via upload

---
+++ DAY3/02_override2.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+class Animal
+{
+    public void Cry() { WriteLine("1. Animal Cry"); }
+}
+class Dog : Animal
+{
+    public new void Cry() { WriteLine("2. Dog Cry"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Animal a = new Animal();
+        Dog    d = new Dog();
+
+        Animal ad = d;
+
+        // 핵심 : 아래 한줄은 어느 함수를 호출할까요 ?
+        ad.Cry(); // ?
+    }
+}
