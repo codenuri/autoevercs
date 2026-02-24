ed2de8cd939e1557b215e1aa94dc1964fa70b8bf
ecourse <smkang@codenuri.co.kr>
2026-02-24 11:44:53 +0900

    Add files via upload

---
+++ DAY3/03_example9.cs
@@ -0,0 +1,20 @@
+﻿abstract class Shape
+{
+    public abstract void Draw();
+}
+
+class Rect : Shape
+{
+    // 규칙 #2. 추상 클래스에서 상속 받는 경우
+    //          반드시 추상 메소드 구현해야 합니다. 
+    //          만들지 않으면 에러!
+    public override void Draw() { }
+}
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
