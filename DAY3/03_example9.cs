f7357c30fd4149006563dc250b6e0b9732906724
ecourse <smkang@codenuri.co.kr>
2026-02-24 11:47:53 +0900

    Add files via upload

--- DAY3/03_example9.cs
+++ DAY3/03_example9.cs
@@ -1,5 +1,8 @@
 ﻿abstract class Shape
 {
+    // #1. 추상 메소드 의미
+    // => 파생 클래스(모든 도형)이 반드시 만들어야 하는 규칙을 설계하는 것
+    // => 구현이 아닌, 메소드 이름을 약속
     public abstract void Draw();
 }
 
@@ -15,6 +18,12 @@ class Program
 {
     public static void Main()
     {
+        // 규칙 #3. 추상 클래스는 객체 생성 안됩니다.
+        // => 메소드 구현이 없으므로
+        Shape s = new Shape(); // error. 컴파일 에러
+
+        // 규칙 #4. 추상 클래스라도 참조 타입으로는 사용가능
+        Shape s2 = new Rect(); // ok
 
     }
 }
