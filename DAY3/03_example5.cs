3eed07f0f456caac9e8ec7a8108d07ca6cdbbf2d
ecourse <smkang@codenuri.co.kr>
2026-02-24 10:56:21 +0900

    Add files via upload

---
+++ DAY3/03_example5.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+
+// 핵심 #5. 
+
+class Shape
+{
+    private int color = 0;
+}
+
+
+
+class Rect : Shape
+{
+    public void Draw() { WriteLine("Draw Rect"); }
+}
+
+class Circle : Shape
+{
+    public void Draw() { WriteLine("Draw Circle"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Shape> s = new List<Shape>();
+
+        while (true)
+        {
+            int cmd = int.Parse(Console.ReadLine());
+
+            if (cmd == 1)
+            {
+                s.Add(new Rect());
+            }
+            else if (cmd == 2)
+            {
+                s.Add(new Circle());
+            }
+            else if (cmd == 9)
+            {
+                foreach (var e in s)
+                {
+                    e.Draw(); 
+                }
+            }
+        }
+    }
+}
+
+
