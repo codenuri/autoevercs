aa3f3df8368826ea33c2910e5cb2832e707657d8
ecourse <smkang@codenuri.co.kr>
2026-02-25 17:15:57 +0900

    Add files via upload

--- DAY4/this2.cs
+++ DAY4/this2.cs
@@ -1,57 +1,37 @@
-﻿// this1.cs   추가
-using System.ComponentModel;
-using static System.Console;
-
-class Point
+﻿class Point
 {
     private int x = 0;
     private int y = 0;
     public Point(int a, int b) => (x, y) = (a, b);
 
-    // 사용자 코드                
-    public void Set(int x, int y)
-    {           
-        // 필드와 인자의 이름이 동일하면 가까운 것 사용
-        // => 아래 코드는 인자의 x, y
-        //x = x; 
-        //y = y;
+    public void Set(int a, int b)   // void Set(Point this, int a, int b)
+    {                               // {
+        x = a;                      //      this.x = a;
+        y = b;                      //      this.y = b;
 
-        // this 활용 #1. 이름 충돌이 있을때 필드임을 나타내기 위해
-        // => 이름 충돌이 없어도, "this.x" 로 사용하면
-        //    필드임을 명확히 표현할수 있으므로 가독성 증가
-        this.x = x;
-        this.y = y;
     }
 
-    // 활용 #2. this 를 반환하면 method 를 연속으로 호출가능
-    public Point SetX(int x)
+    public static void Foo(int a, int b) // void Foo(int a, int b))
     {
-        this.x = x;
-        return this;
-    }
-    public Point SetY(int y)
-    {
-        this.y = y;
-        return this;
+  //      this.x = 10; // error. static method 는 this가 추가안됨
     }
 }
-
 class Program
 {
     public static void Main()
     {
         Point p1 = new Point(1, 2);
-
-        p1.Set(10, 20);
-
-        // 아래 같이 메소드를 연속으로 호출하면서 상태를 변경하는 기술이
-        // JAVA 에서 아주 널리 사용됩니다
-        // Java 용어중 "빌더" 라는 기술
-        p1.SetX(10).SetY(20).SetX(5);
-
-        // 다른 언어는 "method chaining" 이라고도 합니다.
-        // => Rust 에서 널리 사용
-  
+        
+        p1.Set(10, 20);     // Set(p1, 10, 20)
+        Point.Foo(1, 2);    // Foo(10, 20)
 
     }
 }
+
+// instance method : this 추가 됨
+// static  method : this 추가 안됨
+
+// Main 메소드는 static
+// => 즉, Program 객체를 만들지 않고 호출된것
+// => Main 안에서 this 는 없다!!!
+// => 객체가 필요하면 만들어야 한다
