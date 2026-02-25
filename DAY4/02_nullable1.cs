5ba3eb18a3b01dd3b916e2fa5395b03084835eb9
ecourse <smkang@codenuri.co.kr>
2026-02-25 09:44:08 +0900

    Add files via upload

--- DAY4/02_nullable1.cs
+++ DAY4/02_nullable1.cs
@@ -13,9 +13,20 @@ string s2 = null; // ok
 int n1 = 0;     // ok
 int n2 = null;	// error.
 
-// 그래서, value type 은 "값 없음"을 표기할수 없었습니다.
+// #3. 그래서, value type 은 "값 없음"을 표기할수 없었습니다.
 // C#2.0 부터 value type 도 값 없음을 표현하기 위해 Nullable 도입
 
-Nullable<int> n3 = null; // ok. null 가능한 int
+Nullable<int>    n3 = null; // ok. null 가능한 int
+Nullable<double> n4 = null; // ok
 
+// Nullable<string> n5 = null; // error. Nullable<> 안에는 value type만가능
 
+// #4. 단축 표기법
+// => 아래 2줄은 동일한 표기법
+Nullable<int> n5 = null;
+int? n6 = null;  // <== 실전에서는 이렇게 사용
+
+// #5. Nullable 의 원리 - 55 page n1, n2 그림 참고
+int n7 = 10;
+Nullable<int> n8 = null;
+Nullable<int> n9 = 10;
