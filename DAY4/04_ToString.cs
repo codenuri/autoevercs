18ebc50dfdfc27db5ea2d44305b9ca382b8c18c4
ecourse <smkang@codenuri.co.kr>
2026-02-25 11:37:50 +0900

    Add files via upload

--- DAY4/04_ToString.cs
+++ DAY4/04_ToString.cs
@@ -12,6 +12,22 @@ class Object
 }
 */
 
+// ToString() 메소드
+// => 디버깅 등을 위해 객체의 상태를 문자열로 반환 하는 용도로 사용하는 메소드
+// => Object 기본 구현은 "타입 이름" 반환
+// => 보통 클래스 만드는 사람이 override 해서 자신의 상태를 반환하도록 변경
+/*
+class Object
+{
+	// virtual method - 3개
+	// => 클래스 설계자가 다시 만들어도 된다는 의미
+
+	// non-virtual
+	// => 다시 만들지 말고, 제공해주는 기능을 사용해라
+	// => GetType() 등
+}
+*/
+
 class Point 
 {
 	private int x = 0;
@@ -25,7 +41,6 @@ class Point
 		// => $ 문자열을 WriteLine() 뿐 아니라 아래 처럼도 사용가능
 
 		string s = $"x={x}, y={y}";
-
 		return s;
     }
 }
