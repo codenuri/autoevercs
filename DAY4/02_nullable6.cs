469dad8a669af72fcc4483a57a97ca4f10c44424
ecourse <smkang@codenuri.co.kr>
2026-02-25 10:36:23 +0900

    Add files via upload

--- DAY4/02_nullable6.cs
+++ DAY4/02_nullable6.cs
@@ -17,5 +17,14 @@ var n2 = name2.Length; // 실행시간 에러 발생
 // => ~ C#8.0    : 항상 null 이 될수 있다
 // => C#9.0 이후 : reference type 도 null 가능, 불가능을 선택할수 있다
 
-string  s3 = null; // null 불가능 문자열
+string  s3 = null; // ~ C#8.0 까지는 아무 문제 없는 코드
+                   // C#9.0 부터는 null 불가능 문자열
+
 string? s4 = null; // null 가능   문자열
+
+// C# 9.0 이후에 Reference 타입도 ? 를 사용하자는 개념 추가
+// => 사용하지 않아도 에러는 아님.
+// => 경고로 처리
+
+// 이문법을 사용하지 않으면
+// => 프로젝트 설정에서 "<nullable>" 항목을 disable 로 변경
