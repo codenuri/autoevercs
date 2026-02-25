63b1e4827463740b2cf959c0151f3288b6a2d2e1
ecourse <smkang@codenuri.co.kr>
2026-02-25 14:48:21 +0900

    Add files via upload

--- DAY4/06_interface4.cs
+++ DAY4/06_interface4.cs
@@ -11,7 +11,16 @@ class Label : IComparable
 	
 	public Label(string s) => title = s;
 
+    public int CompareTo(object obj)
+    {
+		Label other = (Label)obj;
+
+		// 멤버가 title 한개인데, title 자체는 string 이므로
+		// => CompareTo 있음
+		return title.CompareTo(other.title);
+    }
 }
+
 
 class Program 
 {
@@ -21,7 +30,15 @@ class Program
 		Label d2 = new Label("BAD");
 
 		// 사용자 정의 타입인 Label 도 크기 비교가 되도록 해봅시다.
-		int ret = d1.compare_to(d2);
+		int ret = d1.CompareTo(d2);
+
+		// CompareTo 가 있어도
+		// IComparable 구현하겠다고 표기하지 않으면
+		M1(d1);  // 이 코드가 에러
 	}
+
+    public static void M1(IComparable ic)
+    {
+    }
 }
 
