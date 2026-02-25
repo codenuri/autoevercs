75df76a741ccf8aeb1e3f030c224c778ac517328
ecourse <smkang@codenuri.co.kr>
2026-02-25 17:09:43 +0900

    Add files via upload

---
+++ DAY4/08_delegate_method4.cs
@@ -0,0 +1,26 @@
+using static System.Console;
+
+// multicast 
+
+delegate void MyFunc(int arg);
+
+class Program
+{
+	public static void SMethod(int arg) => WriteLine("Program.SMethod");
+
+	public static void Main()
+	{	
+		// 여러개 함수 등록가능
+		MyFunc f = Test.SMethod;
+
+		f += Program.SMethod;
+
+		f(10); 
+	}
+}
+
+class Test
+{
+    public static void SMethod(int arg) => WriteLine("Test.SMethod");
+    public void IMethod(int arg) => WriteLine("Test_Object.IMethod");
+}
