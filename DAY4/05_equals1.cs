c779a59192ae3e7b8c3974894de3f2257639b151
ecourse <smkang@codenuri.co.kr>
2026-02-25 11:51:52 +0900

    Add files via upload

--- DAY4/05_equals1.cs
+++ DAY4/05_equals1.cs
@@ -1,10 +1,37 @@
 using static System.Console;
 
+/*
+class Object
+{
+	public virtual bool Equals(Object obj)
+	{	
+		// Object 를 만들때는 Point 클래스 를 알수 없습니다.
+		// => 그래서 여기서 동일 상태 를 조사하는 코드는 만들수 없습니다.
+		// => 그래서 기본 구현은 "동일 객체" 조사
+		// => "동일 상태 조사"가 필요하면 파생 클래스(Point) 설계자가
+		//     override 해서 하라는 의미
+
+		// 아래 처럼 되어 있습니다.
+		// => == 연산 사용하므로 "동일 객체" 조사
+		// return obj == this(자기자신)
+	}
+}
+*/
+
 class Point 
 {
 	private int x = 0;
 	private int y = 0;
 	public Point(int a, int b) => (x, y) = (a, b);
+
+    public override bool Equals(object obj)
+    {
+		// Object 타입으로는 x, y 접근 안되므로 Point 타입으로 캐스팅후조사
+        Point pt = (Point)obj;
+
+		// 상태로 동일성 조사
+		return pt.x == x && pt.y == y;	
+    }
 }
 
 class Program
@@ -29,6 +56,7 @@ class Program
 
         // #2. Object 클래스가 제공하는 Equals() 메소드
 
+		// Object 클래스 기본 구현 : 동일 객체 조사
         Console.WriteLine($"{p1.Equals(p2)}");
         Console.WriteLine($"{p3.Equals(p4)}");
     }
