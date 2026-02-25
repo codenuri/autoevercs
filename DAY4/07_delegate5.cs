869fe14f6260a8f0854b1b10f46ab853440490a6
ecourse <smkang@codenuri.co.kr>
2026-02-25 16:13:35 +0900

    Add files via upload

--- DAY4/07_delegate5.cs
+++ DAY4/07_delegate5.cs
@@ -8,10 +8,19 @@ class Program
 {
     public static void Main()
     {
+        // Delegate 변수에는 여러개 함수 등록 가능합니다.
         MyFunc f = Foo;
 
+        f += Goo;
+        f += Foo; // 동일 함수를 2번 등록
+
+        f(10); // Foo, Goo, Foo 호출(등록순)
+
+        f -= Foo;
         f(10);
     }
+
+
     public static void Foo(int arg)
     {
         WriteLine($"Foo : {arg}");
