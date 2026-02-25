46b242ad8c49204f56e50f0cc5e229513a8135c9
ecourse <smkang@codenuri.co.kr>
2026-02-25 15:06:45 +0900

    Add files via upload

---
+++ DAY4/this1.cs
@@ -0,0 +1,18 @@
+﻿// this1.cs   추가
+using static System.Console;
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+
+    }
+}
