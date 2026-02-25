9e52627e888c658c8425b02d172dd5d159581782
ecourse <smkang@codenuri.co.kr>
2026-02-25 16:40:24 +0900

    Add files via upload

---
+++ DAY4/08_delegate_method2-1.cs
@@ -0,0 +1,18 @@
+﻿// 인스턴스 메소드(static 이 아닌 메소드)
+// "객체.메소드이름()" 으로만 호출가능
+
+// 컴파일시 아래 한줄 추가되어서 실행 시작
+// "Program.Main()"
+
+class Program
+{
+    public void F1(int a) 
+    { 
+    }
+    public void F2(int a) { }
+
+    public static void Main()
+    {
+
+    }
+}
