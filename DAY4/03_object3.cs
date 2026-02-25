b768477115ad5e8125a60ea93a6a8a2db9cc8132
ecourse <smkang@codenuri.co.kr>
2026-02-25 11:05:51 +0900

    Add files via upload

---
+++ DAY4/03_object3.cs
@@ -0,0 +1,29 @@
+﻿// 03_object3.cs
+class Program
+{
+    public static void Main()
+    {
+        int n = 10;
+        double d = 3.14;
+
+        PrintHierachy(n);
+        PrintHierachy(d);
+        PrintHierachy("A");
+    }
+
+    public static void PrintHierachy(object obj)
+    {
+        Type t = obj.GetType();
+
+        while (true)
+        {
+            Console.Write("{0} ->", t.Name);
+
+            if (t.Name == "Object") break;
+
+            t = t.BaseType;
+        }
+
+        Console.WriteLine(""); // 개행
+    }
+}
