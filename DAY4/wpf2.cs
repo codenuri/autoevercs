9c5dac6b634e6e3ce07f4fda50682e17eca63f56
ecourse <smkang@codenuri.co.kr>
2026-02-25 17:28:52 +0900

    Add files via upload

---
+++ DAY4/wpf2.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+class MainWindow : Window
+{
+    private Button btn1 = null;
+    private Button btn2 = null;
+    private TextBox tb = null;
+    private Slider sd = null;
+
+    public MainWindow()
+    {
+        StackPanel sp = new StackPanel();
+        this.Content = sp;   // this 없어도 됩니다.
+        
+        //-------------------------------
+        
+    }
+}
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainWindow w = new MainWindow();
+        Application app = new Application();
+
+        w.Show();
+        app.Run();
+    }
+
+}
