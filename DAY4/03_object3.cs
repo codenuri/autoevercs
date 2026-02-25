86022b78056597b1ba55e05996ac153435c17b0f
ecourse <smkang@codenuri.co.kr>
2026-02-25 11:10:22 +0900

    Add files via upload

--- DAY4/03_object3.cs
+++ DAY4/03_object3.cs
@@ -13,9 +13,20 @@ class Program
 {
     public static void Main()
     {
-        List<Rect> s = new List<Rect>();
+        List<Rect> s = new List<Rect>(); // Rect 만 보관 가능
+
+        // 모든 도형을 같이 보관하려면
+        // #1. Object 타입으로 보관
+        // => 단점 : Object 안에는 Draw() 가 없다. Object 타입으로 호출안됨
+        // => 호출하려면 캐스팅 필요하고, 복잡해진다
+        List<object> s1 = new List<object>();
+
+        // #2. 모든 도형의 공통의 특징을 담은 Shape 만들고 Shape 타입으로 보관
+        List<Shape> s2 = new List<Shape>();
 
     }
+
+
 
     public static void PrintHierachy(object obj)
     {
