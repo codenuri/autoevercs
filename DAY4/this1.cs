0fc19c6c978c397325c87b3dd6a6a692a896f21b
ecourse <smkang@codenuri.co.kr>
2026-02-25 15:08:26 +0900

    Add files via upload

--- DAY4/this1.cs
+++ DAY4/this1.cs
@@ -6,6 +6,12 @@ class Point
     private int x = 0;
     private int y = 0;
     public Point(int a, int b) => (x, y) = (a, b);
+
+    public void Set(int a, int b)
+    {
+        x = a; 
+        y = b; 
+    }
 }
 
 class Program
@@ -13,6 +19,10 @@ class Program
     public static void Main()
     {
         Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+
+        p1.Set(10, 20); // 이 한줄의 정확한 원리는 생각해 봅시다
+        p2.Set(10, 20); 
 
     }
 }
