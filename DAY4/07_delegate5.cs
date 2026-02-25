2ff19990d17eb07c0d0ae4ddf1b16c0fbf3354bd
ecourse <smkang@codenuri.co.kr>
2026-02-25 16:12:15 +0900

    Add files via upload

---
+++ DAY4/07_delegate5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+
+delegate void MyFunc(int arg);
+
+class Program
+{
+    public static void Main()
+    {
+        MyFunc f = Foo;
+
+        f(10);
+    }
+    public static void Foo(int arg)
+    {
+        WriteLine($"Foo : {arg}");
+    }
+    public static void Goo(int arg)
+    {
+        WriteLine($"Goo : {arg}");
+    }
+}
