ca9fb7a317e7794dad67f5a9daba435c0b8df158
ecourse <smkang@codenuri.co.kr>
2026-02-25 15:27:17 +0900

    Add files via upload

--- DAY4/this2.cs
+++ DAY4/this2.cs
@@ -11,9 +11,28 @@ class Point
     // 사용자 코드                
     public void Set(int x, int y)
     {           
-        x = x; 
-        y = y; 
+        // 필드와 인자의 이름이 동일하면 가까운 것 사용
+        // => 아래 코드는 인자의 x, y
+        //x = x; 
+        //y = y;
 
+        // this 활용 #1. 이름 충돌이 있을때 필드임을 나타내기 위해
+        // => 이름 충돌이 없어도, "this.x" 로 사용하면
+        //    필드임을 명확히 표현할수 있으므로 가독성 증가
+        this.x = x;
+        this.y = y;
+    }
+
+    // 활용 #2. this 를 반환하면 method 를 연속으로 호출가능
+    public Point SetX(int x)
+    {
+        this.x = x;
+        return this;
+    }
+    public Point SetY(int y)
+    {
+        this.y = y;
+        return this;
     }
 }
 
