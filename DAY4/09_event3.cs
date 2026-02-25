a9f28f7ee8dc1d124896694e0ad79566e71717cb
ecourse <smkang@codenuri.co.kr>
2026-02-25 17:23:21 +0900

    Add files via upload

--- DAY4/09_event3.cs
+++ DAY4/09_event3.cs
@@ -4,7 +4,13 @@ delegate void Handler();
 
 class Button
 {
-    public Handler Click = null;
+    //  public Handler Click = null;
+
+
+    // event 가 없으면 : =, +=, -= 모두 사용가능
+    // event 가 있으면 : +=, -= 만 가능, = 안됨
+
+    public event Handler Click = null;
 
     public void UserPressButton()
     {
@@ -22,7 +28,9 @@ class Program
         Button btn1 = new Button();
 
         btn1.Click = Foo;
-        btn1.Click += Goo;
+//      btn1.Click += Goo;
+        btn1.Click = Goo; // += 하려고 했는데, 실수 했다  
+                          // 이전에 등록한 함수가 모두 제거된다.
 
         btn1.UserPressButton();  
 
