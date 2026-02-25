ef99332956235270cbf2e360d8002bfb71f98881
ecourse <smkang@codenuri.co.kr>
2026-02-25 15:22:29 +0900

    Add files via upload

---
+++ DAY4/this2.cs
@@ -0,0 +1,30 @@
+﻿// this1.cs   추가
+using System.ComponentModel;
+using static System.Console;
+
+class Point
+{
+    private int x = 0;
+    private int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+
+    // 사용자 코드                
+    public void Set(int x, int y)
+    {           
+        x = x; 
+        y = y; 
+
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 2);
+
+        p1.Set(10, 20);    
+  
+
+    }
+}
