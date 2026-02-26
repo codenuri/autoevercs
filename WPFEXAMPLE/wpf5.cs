d0231ffc85577e570dfa3bc10fd047a41f627d95
ecourse <smkang@codenuri.co.kr>
2026-02-26 14:33:00 +0900

    Add files via upload

--- WPFEXAMPLE/wpf5.cs
+++ WPFEXAMPLE/wpf5.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Markup;
 using System.Windows.Media;
 
 // step5. UI 를 XML 로 구축
@@ -13,9 +14,13 @@ class MainWindow : Window
         this.Title = "Hello, WPF";
 
         //--------------------------------------------------
+        // 실행파일 위치 기준으로 ui.txt 위치 : "../../../ui1.txt"
+
+        // #1. 파일만 오픈
         FileStream fs = new FileStream("../../../ui1.txt", 
                                        FileMode.Open, FileAccess.Read);
 
+        // #2. 파일의 내용(XML)에 참고해서 컨트롤 생성
         Button btn = (Button)XamlReader.Load(fs);
 
         fs.Close();
