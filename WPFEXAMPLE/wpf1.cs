73b95b24f16871b5eb0d383addfdc646672d0027
ecourse <smkang@codenuri.co.kr>
2026-02-26 14:04:32 +0900

    Add files via upload

---
+++ WPFEXAMPLE/wpf1.cs
@@ -0,0 +1,17 @@
+﻿// DAY5 안의 WPFSAMPLE.cs 복사
+
+using System.Windows;
+using System.Windows.Media;
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window w = new Window();
+        w.Show();
+
+        Application app = new Application();
+        app.Run();
+    }
+}
