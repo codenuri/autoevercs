a779b395979743ba6c52054e74eca739439b4e77
ecourse <smkang@codenuri.co.kr>
2026-02-26 15:16:39 +0900

    Add files via upload

--- WPFEXAMPLE/wpf8.cs
+++ WPFEXAMPLE/wpf8.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Markup;
 using System.Windows.Media;
 
-// step8
+// step8. Window 가 아닌 사용자가 만든 "MainWindow" 클래스 사용하기
 
 namespace AAA
 {
@@ -43,3 +43,12 @@ namespace AAA
         }
     }
 }
+
+// 정리
+// UI 를 만들기 위해 사용하는 XML 
+// => "XAML" 이라고 하고(발음 자믈)
+// => 관례적으로 .xaml 사용
+
+// 하나의 창은 2개의 파일로 구성 
+// .xaml : UI 를 만들기 위한 XML 코드
+// .cs   : UI 에서 발생하는 이벤트를 처리하기 위한 C# 코드. 위 코드의 Foo 함수
