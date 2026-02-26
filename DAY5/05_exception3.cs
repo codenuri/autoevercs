a4d0d8de20de4ee6dea25c41cdbca58025387852
ecourse <smkang@codenuri.co.kr>
2026-02-26 09:24:37 +0900

    Add files via upload

---
+++ DAY5/05_exception3.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+// 객체지향 언어들의 오류 처리 기술
+// => 예외(exception) 이라는 기술 사용
+
+
+class Database
+{
+    public Database(string dbname) { }
+
+    public void Backup()
+    {
+        bool success = false;
+
+        if (success == false)
+        {
+            throw new Exception(); // 실패 하면 예외를 던진다
+        }
+
+        
+    }
+
+    public void Remove() => WriteLine("Remove DB");
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Database db = new Database("product.db");
+
+        bool ret = db.Backup();
+
+        //if ( ret == false)  { }
+
+        db.Remove();
+    }
+}
