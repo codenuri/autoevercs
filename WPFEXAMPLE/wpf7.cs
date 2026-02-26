8f868cacb3d29c28255d2da60da9e116eb3affb6
ecourse <smkang@codenuri.co.kr>
2026-02-26 14:48:09 +0900

    Add files via upload

---
+++ WPFEXAMPLE/wpf7.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+// step7
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.Title = "Hello, WPF";
+
+
+
+
+    }
+}
+
+class App : Application
+{
+    [STAThread]
+    public static void Main()
+    {
+
+        //--------------------------------------------------
+        FileStream fs = new FileStream("../../../ui3.txt",
+                                       FileMode.Open, FileAccess.Read);
+
+        Window w = (Window)XamlReader.Load(fs);
+
+        fs.Close();
+        //--------------------------------------------------
+
+        w.Show();
+
+        App app = new App();
+        app.Run();
+    }
+}
