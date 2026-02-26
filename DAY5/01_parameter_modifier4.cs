d1c1373683e823fd5d530db373f794344d658d50
ecourse <smkang@codenuri.co.kr>
2026-02-26 10:35:57 +0900

    Add files via upload

--- DAY5/01_parameter_modifier4.cs
+++ DAY5/01_parameter_modifier4.cs
@@ -3,14 +3,13 @@ using static System.Console;
 class MyUtil
 {
 	// Swap 만드세요
-	public static void Swap(int a, int b)
+	public static void Swap(ref int a, ref int b)
 	{
 		int t = a;
 		a = b;
 		b = t;
 	}
 }
-
 class Program
 {
     public static void Main()
@@ -18,7 +17,7 @@ class Program
 		int x = 1;
 		int y = 2;
 
-        MyUtil.Swap( x, y); 
+        MyUtil.Swap( ref x, ref y); 
 
 		// 아래 결과가 2, 1이 나오도록 Swap 을 만들어 보세요
 		WriteLine($"{x}, {y}");	// 2, 1
