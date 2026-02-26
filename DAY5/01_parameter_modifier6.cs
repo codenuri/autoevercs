f107e570513afa7aa6c9d04fee19e804ffd156e5
ecourse <smkang@codenuri.co.kr>
2026-02-26 11:11:01 +0900

    Add files via upload

---
+++ DAY5/01_parameter_modifier6.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+class CPoint
+{
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+}
+
+
+class Program
+{
+    public static void F1(CPoint pt)
+    {
+        pt.X = 10;
+        pt.Y = 20;
+    }
+    public static void Main()
+    {
+
+    }
+}
