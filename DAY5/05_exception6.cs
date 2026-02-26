00626417e857fb2332565d70c5bd090082d36517
ecourse <smkang@codenuri.co.kr>
2026-02-26 09:47:52 +0900

    Add files via upload

--- DAY5/05_exception6.cs
+++ DAY5/05_exception6.cs
@@ -1,17 +1,22 @@
 ﻿
 int age = 0;
 
-Console.Write("나이를 입력해 주세요 >> ");
-
-string s = Console.ReadLine();   // "aa" 입력해 보세요
-
-try
+while(true)
 {
-    age = int.Parse(s);
-}
-catch( FormatException e)
-{
-    Console.WriteLine("잘못 입력 했습니다. 다시 입력해 주세요");
+    Console.Write("나이를 입력해 주세요 >> ");
+
+    string s = Console.ReadLine();   // "aa" 입력해 보세요
+
+    try
+    {
+        age = int.Parse(s);
+    }
+    catch( FormatException e)
+    {
+        Console.WriteLine("잘못 입력 했습니다. 다시 입력해 주세요");
+        continue; // 루프의 시작으로 이동
+    }
+    break; // 예외가 없었다면 루프 탈출!
 }
 
 Console.WriteLine($"입력된 나이는 {age} 입니다");
