90b01f4498178f92fbfd0ed17a698ce5b68b6978
ecourse <smkang@codenuri.co.kr>
2026-02-26 09:22:46 +0900

    Add files via upload

--- DAY5/05_exception2.cs
+++ DAY5/05_exception2.cs
@@ -5,6 +5,13 @@ using static System.Console;
 
 // C 언어 : 반환값으로 실패를 전달
 
+// 단점 : 호출자가 발생된 오류를 무시할수 있다
+//       => 오류를 무시하고 계속 실행되면 더 큰 문제 발생
+//       => DB 백업에 실패 했는데... DB 가 제거됨
+
+// 호출자에게 반드시 오류를 처리하도록 강제 할수 없을까 ?
+// => 오류 처리를 생략하면 프로그램 종료
+
 class Database
 {
     public Database(string dbname) { }
@@ -30,7 +37,7 @@ class Program
 
         bool ret = db.Backup();
 
-        if ( ret == false)  { }
+        //if ( ret == false)  { }
 
         db.Remove();
     }
