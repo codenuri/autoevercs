3c16f1f5585400e4b1acfc26190a197feef21542
ecourse <smkang@codenuri.co.kr>
2026-02-26 15:13:57 +0900

    Add files via upload

--- WPFEXAMPLE/wpf8.cs
+++ WPFEXAMPLE/wpf8.cs
@@ -6,32 +6,40 @@ using System.Windows.Media;
 
 // step8
 
-class MainWindow : Window
+namespace AAA
 {
-    public MainWindow()
+    class MainWindow : Window
     {
-        this.Title = "Hello, WPF";
+        public MainWindow()
+        {
+            this.Title = "Hello, WPF";
 
+        }
+        protected void Foo(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
     }
-}
 
-class App : Application
-{
-    [STAThread]
-    public static void Main()
+
+    class App : Application
     {
-        //--------------------------------------------------
-        FileStream fs = new FileStream("../../../ui4.txt",
-                                       FileMode.Open, FileAccess.Read);
+        [STAThread]
+        public static void Main()
+        {
+            //--------------------------------------------------
+            FileStream fs = new FileStream("../../../ui4.txt",
+                                           FileMode.Open, FileAccess.Read);
 
-        Window w = (Window)XamlReader.Load(fs);
+            Window w = (Window)XamlReader.Load(fs);
 
-        fs.Close();
-        //--------------------------------------------------
+            fs.Close();
+            //--------------------------------------------------
 
-        w.Show();
+            w.Show();
 
-        App app = new App();
-        app.Run();
+            App app = new App();
+            app.Run();
+        }
     }
 }
